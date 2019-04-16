b583392ac0a8c5cc6d6702517f9ac32b2e9db66f
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-04-16 15:21:41 +0200

    redis logging

--- beneath-management/src/lib/redis.ts
+++ beneath-management/src/lib/redis.ts
@@ -2,10 +2,17 @@ import redis from "redis";
 import logger from "./logger";
 
 const url = process.env.REDIS_URL;
-const client = redis.createClient(url);
-
-client.on("error", (err: any) => {
-  logger.error("Redis error", err);
-});
+let client = null;
+if (url) {
+  client = redis.createClient(url);
+  client.on("ready", () => {
+    logger.info("Redis is running");
+  });
+  client.on("error", (err: any) => {
+    logger.error("Redis error", err);
+  });
+} else {
+  logger.info(`Running without Redis`);
+}
 
 export default client;
