e7fac3558a54600a5050f0efb4054e4abba469aa
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-05-11 14:35:14 +0200

    loading projects

--- beneath-frontend/pages/project.js
+++ beneath-frontend/pages/project.js
@@ -1,15 +1,45 @@
+import gql from "graphql-tag";
+import React, { Component } from "react";
 import { withRouter } from "next/router";
+import { Query } from "react-apollo";
 
+import Container from "@material-ui/core/Container";
 import ExploreSidebar from "../components/ExploreSidebar";
+import Loading from "../components/Loading";
 import Page from "../components/Page";
+
+const QUERY_PROJECT = gql`
+  query Project($name: String) {
+    project(name: $name) {
+      projectId
+      name
+      displayName
+      site
+      description
+      createdOn
+      updatedOn
+      users {
+        name
+        photoUrl
+      }
+    }
+  }
+`;
 
 const ProjectPage = ({ router }) => (
   <Page title="Project" sidebar={<ExploreSidebar />}>
-    <div className="section">
-      <div className="title">
-        <h1>Project {router.query.name}</h1>
-      </div>
-    </div>
+    <Container maxWidth="lg">
+      <Query query={QUERY_PROJECT} variables={{ name: router.query.name }}>
+        {({ loading, error, data }) => {
+          if (loading) return <Loading justify="center" />;
+          if (error) return <p>Error: {JSON.stringify(error)}</p>;
+          let { project } = data;
+          return (
+            <p>{JSON.stringify(project)}</p>
+          );
+        }}
+      </Query>
+    </Container>
   </Page>
 );
 
