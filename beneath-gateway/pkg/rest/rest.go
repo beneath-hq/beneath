374cb8c33dacf2041b568352545dcacb164cd1d2
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-05-27 22:29:18 +0200

    spec

--- beneath-gateway/pkg/rest/rest.go
+++ beneath-gateway/pkg/rest/rest.go
@@ -16,12 +16,32 @@ func GetHandler() http.Handler {
 	// handler.Get("/projects/{projectName}/graphql")
 
 	// REST endpoints
-	handler.Get("/projects/{projectName}/streams/{streamName}", getStreamHandler)
-	// handler.Post("/projects/{projectName}/streams/{streamName}")
+	handler.Get("/projects/{projectName}/streams/{streamName}", getStream)
+	handler.Post("/projects/{projectName}/streams/{streamName}", postStream)
 
 	return handler
 }
 
-func getStreamHandler(w http.ResponseWriter, r *http.Request) {
+func getStream(w http.ResponseWriter, r *http.Request) {
+	// projectName := chi.URLParam(r, "projectName")
+	// streamName := chi.URLParam(r, "streamName")
+
+	// SPEC
+	// Read Authorization, check token and get user
+	// Check user has read access to stream (or stream is public if not user)
+	// (Read from BT in accordance with how we end up writing it)
+
 	w.Write([]byte("hello world"))
+}
+
+func postStream(w http.ResponseWriter, r *http.Request) {
+	// projectName := chi.URLParam(r, "projectName")
+	// streamName := chi.URLParam(r, "streamName")
+
+	// SPEC
+	// - Read Authorization, check token and get user
+	// - Check user has write access to project
+	// - Get schema for stream
+	// - Read payload (JSON) and encode with schema
+	// - Write to pubsub
 }
