26f9abe0574ff8d3847876dc9b90beb2efdfedd3
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-06-05 14:44:26 +0200

    bugfix and query logging

--- beneath-gateway/pkg/beneath/db.go
+++ beneath-gateway/pkg/beneath/db.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	// DB is the postgres connection
-	DB pg.DB
+	DB *pg.DB
 )
 
 func init() {
@@ -17,6 +17,18 @@ func init() {
 		log.Fatalf("postgres: %s", err.Error())
 	}
 
-	DB := pg.Connect(opts)
-	defer DB.Close()
+	DB = pg.Connect(opts)
+
+	// Uncomment to log database queries
+	// DB.AddQueryHook(queryLoggerHook{})
+}
+
+// queryLoggerHook logs every database query
+type queryLoggerHook struct{}
+
+func (h queryLoggerHook) BeforeQuery(q *pg.QueryEvent) {
+}
+
+func (h queryLoggerHook) AfterQuery(q *pg.QueryEvent) {
+	log.Println(q.FormattedQuery())
 }
