bdc721c3975dabd4330a60beac1ce55057495098
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-06-19 11:36:10 +0200

    proto update

--- beneath-gateway/proto-build.sh
+++ beneath-gateway/proto-build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-protoc -I beneath/beneath_proto/ beneath/beneath_proto/*.proto --go_out=plugins=grpc:beneath/beneath_proto
+protoc -I beneath/proto/ beneath/proto/*.proto --go_out=plugins=grpc:beneath/proto
