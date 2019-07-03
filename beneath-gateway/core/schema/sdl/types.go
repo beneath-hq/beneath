eee69a7b5c7ff91cd8f97528eddd7ccd159ab887
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-07-03 23:08:37 +0200

    check types with regexes

--- beneath-gateway/core/schema/sdl/types.go
+++ beneath-gateway/core/schema/sdl/types.go
@@ -1,33 +1,58 @@
 package sdl
 
+import "regexp"
+
+var (
+	primitiveRegex *regexp.Regexp
+	indexableRegex *regexp.Regexp
+)
+
+func init() {
+	primitiveRegex = regexp.MustCompile(makePrimitiveRegex())
+	indexableRegex = regexp.MustCompile(makeIndexableRegex())
+}
+
+func makePrimitiveRegex() string {
+	return ("^(()" +
+		"|(Boolean)" +
+		"|(Bytes)" +
+		"|(Bytes[1-9][0-9]*)" +
+		"|(Float)" +
+		"|(Float32)" +
+		"|(Float64)" +
+		"|(Int)" +
+		"|(Int32)" +
+		"|(Int64)" +
+		"|(Numeric)" +
+		"|(String)" +
+		"|(Timestamp)" +
+		")$")
+}
+
+func makeIndexableRegex() string {
+	return ("^(()" +
+		"|(Bytes)" +
+		"|(Bytes[1-9][0-9]*)" +
+		"|(Int)" +
+		"|(Int32)" +
+		"|(Int64)" +
+		"|(String)" +
+		"|(Timestamp)" +
+		")$")
+}
+
 // returns true iff primitive
 func isPrimitiveType(tr *TypeRef) bool {
 	if tr.Array != nil {
 		return false
 	}
-
-	// TODO
-
-	return (tr.Type == "Boolean" ||
-		tr.Type == "Int" ||
-		tr.Type == "Int32" ||
-		tr.Type == "Int64" ||
-		tr.Type == "Float" ||
-		tr.Type == "Float32" ||
-		tr.Type == "Float64" ||
-		tr.Type == "Numeric" ||
-		tr.Type == "Timestamp" ||
-		tr.Type == "Bytes" ||
-		tr.Type == "Bytes20" ||
-		tr.Type == "String")
+	return primitiveRegex.MatchString(tr.Type)
 }
 
 // returns true iff can be used in an index or as a key
 func isIndexableType(tr *TypeRef) bool {
-	return isPrimitiveType(tr) && (tr.Type == "Int" ||
-		tr.Type == "Int32" ||
-		tr.Type == "Int64" ||
-		tr.Type == "Timestamp" ||
-		tr.Type == "Bytes" ||
-		tr.Type == "String")
+	if tr.Array != nil {
+		return false
+	}
+	return indexableRegex.MatchString(tr.Type)
 }
