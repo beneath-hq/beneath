810ffb97effadd53819d0a3304f816b123952e05
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-07-08 13:18:20 +0200

    docs

--- beneath-go/control/model/stream_instance.go
+++ beneath-go/control/model/stream_instance.go
@@ -6,6 +6,9 @@ import (
 	uuid "github.com/satori/go.uuid"
 )
 
+// StreamInstance represents a single version of a stream (for a streaming stream,
+// there will only be one instance; but for a batch stream, each update represents
+// a new instance)
 type StreamInstance struct {
 	StreamInstanceID uuid.UUID `sql:",pk,type:uuid,default:uuid_generate_v4()"`
 	StreamID         uuid.UUID `sql:"on_delete:RESTRICT,notnull,type:uuid"`
