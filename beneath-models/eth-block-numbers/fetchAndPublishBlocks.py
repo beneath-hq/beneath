83757ee600597400bf8e26b7eaae6a1937ba038d
Svante Jørgensen <svantetobias@gmail.com>
2019-07-17 14:27:31 +0200

    Model: Extract parent hash. Refactor get newer block number logic, to it's own function.

--- beneath-models/eth-block-numbers/fetchAndPublishBlocks.py
+++ beneath-models/eth-block-numbers/fetchAndPublishBlocks.py
@@ -1,24 +1,34 @@
 from web3 import Web3
 import time
+import requests
 
 PROVIDER_URL = "https://mainnet.infura.io/v3/4e5fbbd54aef484daabd76627d8f5dc9"
-START_FROM_BLOCK_NO = 8167540
+START_FROM_BLOCK_NO = 8168249
 
 web3 = Web3(Web3.HTTPProvider(PROVIDER_URL))
 
 current_block = START_FROM_BLOCK_NO
-newest_block = int(web3.eth.getBlock('latest').number) # TODO: Try/Catch
+newest_block = web3.eth.getBlock('latest')  # TODO: Try/Catch
+print("Newest block:",newest_block)
+newest_block_no = int(newest_block.number)
+
+
+def get_newer_block_no(current_block, newest_block_no):
+  while current_block > newest_block_no:
+      print("Getting newest block number...")
+      block_no = web3.eth.getBlock('latest').number
+      if current_block > block_no:
+          print("No new block(s) found, waiting 5 sec...")
+          time.sleep(5)
+      else:
+          return block_no
+
 
 while True:
-    if current_block > newest_block:
-        while current_block > newest_block:
-          print("Getting newest block number...")
-          newest_block = web3.eth.getBlock('latest').number
-          if current_block > newest_block:
-            print("No new block(s) found, waiting 5 sec...")
-            time.sleep(5)
+    if current_block > newest_block_no:
+      newest_block_no = get_newer_block_no(current_block, newest_block_no)
 
     block = web3.eth.getBlock(current_block)
-    print(f"Block {block.number} hash is {block.hash.hex()}")
+    print(f"Block {block.number} hash is {block.hash.hex()}, with parent hash {block.parentHash.hex()}")
 
     current_block += 1
