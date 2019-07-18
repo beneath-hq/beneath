5b011f9c51fb09ee3be154b2cf765c323fa36cb3
Svante Jørgensen <svantetobias@gmail.com>
2019-07-18 10:37:52 +0200

    Models: Test refactoring and whitespace format according to PEP 8

--- beneath-models/eth-block-numbers/fetch_and_publish_blocks.py
+++ beneath-models/eth-block-numbers/fetch_and_publish_blocks.py
@@ -1,24 +1,27 @@
-from web3 import Web3
 import time
+
 import requests
+from web3 import Web3
 
 PROVIDER_URL = "https://mainnet.infura.io/v3/4e5fbbd54aef484daabd76627d8f5dc9"
-START_FROM_BLOCK_NO = 8168249
-web3 = Web3(Web3.HTTPProvider(PROVIDER_URL))
+START_FROM_BLOCK_NO = 8173297
+w3 = Web3(Web3.HTTPProvider(PROVIDER_URL))
+
 
 def get_newer_block_no(current_block, newest_block_no):
     while current_block > newest_block_no:
         print("Getting newest block number...")
-        block_no = web3.eth.getBlock('latest').number
+        block_no = w3.eth.getBlock('latest').number
         if current_block > block_no:
             print("No new block(s) found, waiting 5 sec...")
             time.sleep(5)
         else:
             return block_no
 
+
 def main():
     current_block = START_FROM_BLOCK_NO
-    newest_block = web3.eth.getBlock('latest')  # TODO: Try/Catch
+    newest_block = w3.eth.getBlock('latest')  # TODO: Try/Catch
     print("Newest block:", newest_block)
     newest_block_no = int(newest_block.number)
 
@@ -26,10 +29,12 @@ def main():
         if current_block > newest_block_no:
             newest_block_no = get_newer_block_no(current_block, newest_block_no)
 
-        block = web3.eth.getBlock(current_block)
-        print(f"Block {block.number} hash is {block.hash.hex()}, with parent hash {block.parentHash.hex()}")
+        block = w3.eth.getBlock(current_block)
+        print(
+            f"Block {block.number} hash is {block.hash.hex()}, with parent hash {block.parentHash.hex()}")
 
         current_block += 1
+
 
 if __name__ == "__main__":
     main()
--- beneath-models/eth-block-numbers/test_fetch_and_publish_blocks.py
+++ beneath-models/eth-block-numbers/test_fetch_and_publish_blocks.py
@@ -1,49 +1,54 @@
 import unittest
-from unittest.mock import call, Mock
-import fetch_and_publish_blocks as fpb
-from web3.datastructures import (AttributeDict)
+from unittest.mock import Mock, call
+
 from hexbytes import HexBytes
+from web3.datastructures import AttributeDict
 
-class TestFetchAndPublish(unittest.TestCase):
+import fetch_and_publish_blocks as fpb
 
+
+class Test_FetchAndPublish(unittest.TestCase):
     def test_get_blocks_with_web3(self):
         fpb.START_FROM_BLOCK_NO = 8123121
 
-        # Mock web3.eth.getBlock() to control its behavior
+        # Mock w3.eth.getBlock() to control its behavior
         mock_getBlock = Mock()
         latest_call_counter = 0
 
         def getBlock_return_values(arg):
+            blocks = {
+                "latest": AttributeDict({"number": 8123123, "hash": HexBytes("0x123"), "parentHash": HexBytes("0x122")}),
+                8123121: AttributeDict({"number": 8123121, "hash": HexBytes("0x121"), "parentHash": HexBytes("0x120")}),
+                8123122: AttributeDict({"number": 8123122, "hash": HexBytes("0x122"), "parentHash": HexBytes("0x121")}),
+                8123123: AttributeDict({"number": 8123123, "hash": HexBytes("0x123"), "parentHash": HexBytes("0x122")}),
+            }
             if arg == "latest":
-                # 
+                # When "latest" is called the third time, raise an exception so we can test results
                 nonlocal latest_call_counter
                 latest_call_counter += 1
                 if latest_call_counter >= 2:
-                   raise Exception('stop loop for test assertion purposes')
+                    raise Exception('stop loop for test assertion purposes')
                 else:
-                    return AttributeDict({"number": 8123123, "hash": HexBytes("0x123"), "parentHash": HexBytes("0x122")})
-            elif arg == 8123121:
-                return AttributeDict({"number": 8123121, "hash": HexBytes("0x121"), "parentHash": HexBytes("0x120")})
-            elif arg == 8123122:
-                return AttributeDict({"number": 8123122, "hash": HexBytes("0x122"), "parentHash": HexBytes("0x121")})
-            elif arg == 8123123:
-                return AttributeDict({"number": 8123123, "hash": HexBytes("0x123"), "parentHash": HexBytes("0x122")})
+                    return blocks["latest"]
+            else:
+                return blocks[arg]
 
         mock_getBlock.side_effect = getBlock_return_values
-        fpb.web3.eth.getBlock = mock_getBlock
-        
+        fpb.w3.eth.getBlock = mock_getBlock
+
         # Run the main loop until the test-exception stops it
         try:
-          fpb.main()
+            fpb.main()
         except Exception as e:
-          print(str(e))
-          pass
-        
-        # Assert that web3.eth.getBlock() was called exactly as we expected
-        fpb.web3.eth.getBlock.assert_has_calls([
+            print(str(e))
+            pass
+
+        # Assert that w3.eth.getBlock() was called exactly as we expected
+        fpb.w3.eth.getBlock.assert_has_calls([
             call("latest"), call(8123121), call(8123122), call(8123123), call("latest")
         ], any_order=False)
-        self.assertEqual(fpb.web3.eth.getBlock.call_count, 5, "must have been called exactly 5 times")
+        self.assertEqual(fpb.w3.eth.getBlock.call_count, 5, "must have been called exactly 5 times")
+
 
 if __name__ == '__main__':
     unittest.main()
