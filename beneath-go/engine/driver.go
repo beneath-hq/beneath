6d786c49f6a451b9dee477c77d714f9751b900c0
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-07-18 10:37:56 +0200

    documented drivers functions

--- beneath-go/engine/driver.go
+++ beneath-go/engine/driver.go
@@ -4,19 +4,26 @@ import pb "github.com/beneath-core/beneath-go/proto"
 
 // StreamsDriver defines the functions necessary to encapsulate Beneath's streaming data needs
 type StreamsDriver interface {
+	// GetMaxMessageSize returns the maximum accepted message size in bytes
 	GetMaxMessageSize() int
-	PushWriteRequest(req *pb.WriteInternalRecordsRequest) error
+
+	// QueueWriteRequest queues a write request -- concretely, it results in
+	// the write request being written to Pubsub, then from there read by
+	// the data processing pipeline and written to BigTable and BigQuery
+	QueueWriteRequest(req *pb.WriteRecordsRequest) error
 }
 
 // TablesDriver defines the functions necessary to encapsulate Beneath's operational datastore needs
 type TablesDriver interface {
+	// GetMaxKeySize returns the maximum accepted key size in bytes
 	GetMaxKeySize() int
+
+	// GetMaxDataSize returns the maximum accepted value size in bytes
 	GetMaxDataSize() int
-	// TODO: Add table functions
 }
 
 // WarehouseDriver defines the functions necessary to encapsulate Beneath's data archiving needs
 type WarehouseDriver interface {
+	// GetMaxDataSize returns the maximum accepted row size in bytes
 	GetMaxDataSize() int
-	// TODO: Add warehouse functions
 }
