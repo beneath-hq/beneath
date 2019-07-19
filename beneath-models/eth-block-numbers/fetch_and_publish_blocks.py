2eea7f26f203a641a3f162d70dad4b93d0cafe90
Svante Jørgensen <svantetobias@gmail.com>
2019-07-19 17:37:39 +0200

    Model: Adapt code to real gateway interface and auth. And fix test side effects by reloading module

--- beneath-models/eth-block-numbers/fetch_and_publish_blocks.py
+++ beneath-models/eth-block-numbers/fetch_and_publish_blocks.py
@@ -4,10 +4,19 @@ import requests
 from web3 import Web3
 
 WEB3_PROVIDER_URL = "https://mainnet.infura.io/v3/4e5fbbd54aef484daabd76627d8f5dc9"
-BENEATH_POST_BLOCK_URL = "https://beneath.network/projects/ethereum/streams/ethereum-blocks"
-BENEATH_GET_LATEST_BLOCK_URL = "https://beneath.network/projects/ethereum/streams/ethereum-blocks?get-latest-record"
-DEFAULT_START_BLOCK_NO = 0
+BENEATH_BASE_URL = "http://localhost:5000"
+BENEATH_PROJECT = "ethereum"
+BENEATH_PROJECT_KEY = "Ipv9X3mW3fwu+qwtfnJyalUdDOvxTtXG00V+NFEjvBg="
+BENEATH_PROJECT_STREAM = "block-numbers"
+
+beneath_stream_url = f"{BENEATH_BASE_URL}/projects/{BENEATH_PROJECT}/streams/{BENEATH_PROJECT_STREAM}"
+beneath_get_latest_block_url = "http://not.working.yet/"  # f"{BENEATH_BASE_URL}/projects/{BENEATH_PROJECT}/streams/{BENEATH_PROJECT_STREAM}?get-latest-record"
+
 w3 = Web3(Web3.HTTPProvider(WEB3_PROVIDER_URL))
+
+
+def get_beneath_instance_url(instance_id):
+    return f"{BENEATH_BASE_URL}/streams/instances/{instance_id}"
 
 
 def current_milli_time():
@@ -15,37 +24,39 @@ def current_milli_time():
 
 
 def get_block_from_gateway(block_number):
-    return requests.get(
-        f"https://beneath.network/projects/ethereum/streams/ethereum-blocks?=block-number={block_number}"
-    ).json()
+    return requests.get(f"{beneath_stream_url}?number={block_number}").json()
 
 
 def get_start_block():
     # First get the most recent block sent to the gateway
-    response = requests.get(BENEATH_GET_LATEST_BLOCK_URL)
+    try:
+        response = requests.get(beneath_get_latest_block_url)
+    except Exception as e:
+        # If gateway cannot be reached, start from block 0
+        return 0
+
     if response.status_code <= 200:
         gateway_block = response.json()
     else:
-        # If gateway fails to respond, start from block 0
+        # If gateway respond with an error, start from block 0
         return 0
 
     # Compare gateway block hash with same blocknumbers hash from web3
-    web3_block = w3.eth.getBlock(gateway_block['blockNumber'])
-    while gateway_block['blockHash'] != web3_block.hash.hex():
+    web3_block = w3.eth.getBlock(gateway_block['number'])
+    while gateway_block['hash'] != web3_block.hash.hex():
         # If hashes don't match, a fork probably happened. Check previous block until hash matches, so we are behind the fork and can go forward again.
         print(
-            f"Warning! Gateway block {gateway_block['blockNumber']} with hash {gateway_block['blockHash']} does not match Web3 block {web3_block.number} with hash {web3_block.hash.hex()}"
+            f"Warning! Gateway block {gateway_block['number']} with hash {gateway_block['hash']} does not match Web3 block {web3_block.number} with hash {web3_block.hash.hex()}"
         )
         print("Trying the previous block...")
-        if gateway_block['blockNumber'] > 0:
-            gateway_block = get_block_from_gateway(
-                gateway_block['blockNumber'] - 1)
-            web3_block = w3.eth.getBlock(gateway_block['blockNumber'])
+        if gateway_block['number'] > 0:
+            gateway_block = get_block_from_gateway(gateway_block['number'] - 1)
+            web3_block = w3.eth.getBlock(gateway_block['number'])
         else:
             raise Exception(
                 "Could not find any block hash in gateway matching Web3 block hashes!"
             )
-    return gateway_block['blockNumber'] + 1
+    return gateway_block['number'] + 1
 
 
 def get_newer_block_no(current_block, newest_block_no):
@@ -59,17 +70,24 @@ def get_newer_block_no(current_block, newest_block_no):
             return block_no
 
 
-def post_block_to_gateway(block_number, block_hash, block_parent_hash):
-    headers = {'content-type': 'application/json'}
-
-    post_json = {
-        "blockNumber": block_number,
-        "blockHash": block_hash,
-        "blockParentHash": block_parent_hash,
-        "syncTimestamp": current_milli_time()
+def post_block_to_gateway(instance_id, block_number, block_hash, block_parent_hash,
+                          block_timestamp):
+    headers = {
+        "Authorization": f"Bearer {BENEATH_PROJECT_KEY}",
+        "content-type": "application/json"
     }
 
-    response = requests.post(BENEATH_POST_BLOCK_URL,
+    post_json = {
+        "@meta": {
+            "sequence_number": current_milli_time()
+        },
+        "number": block_number,
+        "hash": block_hash,
+        "parentHash": block_parent_hash,
+        "timestamp": block_timestamp
+    }
+
+    response = requests.post(get_beneath_instance_url(instance_id),
                              json=post_json,
                              headers=headers)
 
@@ -80,6 +98,10 @@ def post_block_to_gateway(block_number, block_hash, block_parent_hash):
 
 
 def main():
+    instance_id = requests.get(f"{beneath_stream_url}/details",
+                               headers={
+                                   "Bearer": BENEATH_PROJECT_KEY
+                               }).json()["current_instance_id"]
     current_block = get_start_block()
     newest_block = w3.eth.getBlock('latest')  # TODO: Try/Catch
     print("Newest block:", newest_block)
@@ -94,8 +116,8 @@ def main():
             f"Block {block.number} hash is {block.hash.hex()}, with parent hash {block.parentHash.hex()}"
         )
 
-        post_block_to_gateway(block.number, block.hash.hex(),
-                              block.parentHash.hex())
+        post_block_to_gateway(instance_id, block.number, block.hash.hex(),
+                              block.parentHash.hex(), block.timestamp)
 
         current_block += 1
 
