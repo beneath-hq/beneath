587f1ab768333ab68cf0190390b08e68ee6d4fe2
Svante Jørgensen <svantetobias@gmail.com>
2019-07-23 17:08:05 +0200

    Model: Change indentation from 4 to 2 spaces.

--- beneath-models/eth-block-numbers/fetch_and_publish_blocks.py
+++ beneath-models/eth-block-numbers/fetch_and_publish_blocks.py
@@ -20,80 +20,80 @@ W3 = Web3(Web3.HTTPProvider(config.WEB3_PROVIDER_URL))
        stop=stop_after_attempt(5),
        before_sleep=before_sleep_log(LOG, logging.ERROR))
 def get_stream_instance_id():
-    response = requests.get(f"{BENEATH_STREAM_URL}/details",
-                            headers={"Bearer": config.BENEATH_PROJECT_KEY})
-    response.raise_for_status()
-    return response.json()["current_instance_id"]
+  response = requests.get(f"{BENEATH_STREAM_URL}/details",
+                          headers={"Bearer": config.BENEATH_PROJECT_KEY})
+  response.raise_for_status()
+  return response.json()["current_instance_id"]
 
 
 def get_beneath_instance_url(instance_id):
-    return f"{config.BENEATH_BASE_URL}/streams/instances/{instance_id}"
+  return f"{config.BENEATH_BASE_URL}/streams/instances/{instance_id}"
 
 
 def current_milli_time():
-    return int(time.time() * 1000)
+  return int(time.time() * 1000)
 
 
 @retry(wait=wait_random(min=5, max=10),
        stop=stop_after_attempt(5),
        before_sleep=before_sleep_log(LOG, logging.ERROR))
 def get_latest_block_from_gateway():
-    ''' Get the most recent block that was sent to the gateway '''
-    LOG.info("get_latest_block_from_gateway from %s",
-             BENEATH_GET_LATEST_BLOCK_URL)
-    response = requests.get(BENEATH_GET_LATEST_BLOCK_URL)
-    response.raise_for_status()
-    return response.json()
+  ''' Get the most recent block that was sent to the gateway '''
+  LOG.info("get_latest_block_from_gateway from %s",
+           BENEATH_GET_LATEST_BLOCK_URL)
+  response = requests.get(BENEATH_GET_LATEST_BLOCK_URL)
+  response.raise_for_status()
+  return response.json()
 
 
 @retry(wait=wait_random(min=5, max=10),
        stop=stop_after_attempt(5),
        before_sleep=before_sleep_log(LOG, logging.ERROR))
 def get_block_from_gateway(block_number):
-    response = requests.get(f"{BENEATH_STREAM_URL}?number={block_number}")
-    response.raise_for_status()
-    return response.json()
+  response = requests.get(f"{BENEATH_STREAM_URL}?number={block_number}")
+  response.raise_for_status()
+  return response.json()
 
 
 @retry(wait=wait_random(min=5, max=10),
        stop=stop_after_attempt(5),
        before_sleep=before_sleep_log(LOG, logging.ERROR))
 def get_block_from_web3(block):
-    return W3.eth.getBlock(block)
+  return W3.eth.getBlock(block)
 
 
 def get_start_block_no():
-    gateway_block = get_latest_block_from_gateway()
-    # Compare gateway block hash with same blocknumbers hash from web3
-    web3_block = get_block_from_web3(gateway_block['number'])
-    while gateway_block['hash'] != web3_block.hash.hex():
-        # If hashes don't match, a fork probably happened.
-        # Check previous block until hash matches, so we are behind the fork and can go forward again.
-        LOG.warning(
-            "Warning! Gateway block %s with hash %s does not match Web3 block %s with hash %s",
-            gateway_block['number'], gateway_block['hash'], web3_block.number,
-            web3_block.hash.hex())
-        LOG.info("Trying the previous block...")
-        if gateway_block['number'] > 0:
-            gateway_block = get_block_from_gateway(gateway_block['number'] - 1)
-            web3_block = get_block_from_web3(gateway_block['number'])
-        else:
-            raise Exception(
-                "Could not find any block hash in gateway matching Web3 block hashes!"
-            )
-    return gateway_block['number'] + 1
+  gateway_block = get_latest_block_from_gateway()
+  # Compare gateway block hash with same blocknumbers hash from web3
+  web3_block = get_block_from_web3(gateway_block['number'])
+  while gateway_block['hash'] != web3_block.hash.hex():
+    # If hashes don't match, a fork probably happened.
+    # Check previous block until hash matches, so we are behind the fork and can go forward again.
+    LOG.warning(
+        "Warning! Gateway block %s with hash %s does not match Web3 block %s with hash %s",
+        gateway_block['number'], gateway_block['hash'], web3_block.number,
+        web3_block.hash.hex())
+    LOG.info("Trying the previous block...")
+    if gateway_block['number'] > 0:
+      gateway_block = get_block_from_gateway(gateway_block['number'] - 1)
+      web3_block = get_block_from_web3(gateway_block['number'])
+    else:
+      raise Exception(
+          "Could not find any block hash in gateway matching Web3 block hashes!"
+      )
+  return gateway_block['number'] + 1
 
 
 def get_newer_block_no(current_block_no, target_block_no):
-    while current_block_no > target_block_no:
-        LOG.info("Sync finished. Checking for newer block number...")
-        block_no = get_block_from_web3('latest').number
-        if current_block_no > block_no:
-            LOG.info("No new block(s) found, waiting 5 sec...")
-            time.sleep(5)
-        else:
-            LOG.info("New block number found: %s", block_no)
-            return block_no
+  while current_block_no > target_block_no:
+    LOG.info("Sync finished. Checking for newer block number...")
+    block_no = get_block_from_web3('latest').number
+    if current_block_no > block_no:
+      LOG.info("No new block(s) found, waiting 5 sec...")
+      time.sleep(5)
+    else:
+      LOG.info("New block number found: %s", block_no)
+      return block_no
 
 
 @retry(wait=wait_random(min=5, max=10),
@@ -101,58 +101,58 @@ def get_newer_block_no(current_block_no, target_block_no):
        before_sleep=before_sleep_log(LOG, logging.ERROR))
 def post_block_to_gateway(instance_id, block_number, block_hash,
                           block_parent_hash, block_timestamp):
-    headers = {
-        "Authorization": f"Bearer {config.BENEATH_PROJECT_KEY}",
-        "content-type": "application/json"
-    }
+  headers = {
+      "Authorization": f"Bearer {config.BENEATH_PROJECT_KEY}",
+      "content-type": "application/json"
+  }
 
-    post_json = {
-        "@meta": {
-            "sequence_number": current_milli_time()
-        },
-        "number": block_number,
-        "hash": block_hash,
-        "parentHash": block_parent_hash,
-        "timestamp": block_timestamp
-    }
+  post_json = {
+      "@meta": {
+          "sequence_number": current_milli_time()
+      },
+      "number": block_number,
+      "hash": block_hash,
+      "parentHash": block_parent_hash,
+      "timestamp": block_timestamp
+  }
 
-    response = requests.post(get_beneath_instance_url(instance_id),
-                             json=post_json,
-                             headers=headers)
-    response.raise_for_status()
+  response = requests.post(get_beneath_instance_url(instance_id),
+                           json=post_json,
+                           headers=headers)
+  response.raise_for_status()
 
-    LOG.info("Block posted successfully to gateway")
+  LOG.info("Block posted successfully to gateway")
 
 
 def main():
-    instance_id = get_stream_instance_id()
-    LOG.info("Got gateway instance ID: %s", instance_id)
+  instance_id = get_stream_instance_id()
+  LOG.info("Got gateway instance ID: %s", instance_id)
 
-    current_block_no = get_start_block_no()
-    LOG.info("Starting sync from block %s", current_block_no)
+  current_block_no = get_start_block_no()
+  LOG.info("Starting sync from block %s", current_block_no)
 
-    newest_block = get_block_from_web3('latest')
-    target_block_no = int(newest_block.number)
-    LOG.info("Current newest block from Web3 is %s", target_block_no)
+  newest_block = get_block_from_web3('latest')
+  target_block_no = int(newest_block.number)
+  LOG.info("Current newest block from Web3 is %s", target_block_no)
 
-    while True:
-        if current_block_no > target_block_no:
-            target_block_no = get_newer_block_no(current_block_no, target_block_no)
+  while True:
+    if current_block_no > target_block_no:
+      target_block_no = get_newer_block_no(current_block_no, target_block_no)
 
-        block = get_block_from_web3(current_block_no)
+    block = get_block_from_web3(current_block_no)
 
-        LOG.info("Block %s hash is %s, with parent hash %s", block.number,
-                 block.hash.hex(), block.parentHash.hex())
+    LOG.info("Block %s hash is %s, with parent hash %s", block.number,
+             block.hash.hex(), block.parentHash.hex())
 
-        # Keep previous block and compare its hash to the new block's parent hash.
-        # If they are not the same, we have a chain reorg! Log warning and handle it,
-        # by walking backwards and compare with gateway block hashes until they are the same.
+    # Keep previous block and compare its hash to the new block's parent hash.
+    # If they are not the same, we have a chain reorg! Log warning and handle it,
+    # by walking backwards and compare with gateway block hashes until they are the same.
 
-        post_block_to_gateway(instance_id, block.number, block.hash.hex(),
-                              block.parentHash.hex(), block.timestamp)
+    post_block_to_gateway(instance_id, block.number, block.hash.hex(),
+                          block.parentHash.hex(), block.timestamp)
 
-        current_block_no += 1
+    current_block_no += 1
 
 
 if __name__ == "__main__":
-    main()
+  main()
--- beneath-models/eth-block-numbers/test_fetch_and_publish_blocks.py
+++ beneath-models/eth-block-numbers/test_fetch_and_publish_blocks.py
@@ -10,160 +10,159 @@ import fetch_and_publish_blocks as fpb
 
 class Test_FetchAndPublishBlocks(unittest.TestCase):
 
-    def setUp(self):
-        # Reload fetch_and_publish_blocks so no side effects from other tests are carried over
-        importlib.reload(fpb)
+  def setUp(self):
+    # Reload fetch_and_publish_blocks so no side effects from other tests are carried over
+    importlib.reload(fpb)
 
-    def test_get_blocks_with_web3(self):
-        # Mock the current time, so we get predictable results
-        current_time = 123123123
-        fpb.current_milli_time = Mock(return_value=current_time)
-        current_instance_id = "123TestInstanceId"
+  def test_get_blocks_with_web3(self):
+    # Mock the current time, so we get predictable results
+    current_time = 123123123
+    fpb.current_milli_time = Mock(return_value=current_time)
+    current_instance_id = "123TestInstanceId"
 
-        # Mock the latest block synced, so we get predictable results
-        fpb.get_start_block_no = Mock(return_value=8123121)
+    # Mock the latest block synced, so we get predictable results
+    fpb.get_start_block_no = Mock(return_value=8123121)
 
-        # Mock W3.eth.getBlock() to control its behavior
-        mock_get_block = Mock()
-        latest_call_counter = 0
+    # Mock W3.eth.getBlock() to control its behavior
+    mock_get_block = Mock()
+    latest_call_counter = 0
 
-        def getBlock_return_values(*arg, **kwargs):
-            blocks = {
-                "latest":
-                    AttributeDict({
-                        "number": 8123123,
-                        "hash": HexBytes("3123"),
-                        "parentHash": HexBytes("3122"),
-                        "timestamp": 10003
-                    }),
-                8123121:
-                    AttributeDict({
-                        "number": 8123121,
-                        "hash": HexBytes("3121"),
-                        "parentHash": HexBytes("3120"),
-                        "timestamp": 10001
-                    }),
-                8123122:
-                    AttributeDict({
-                        "number": 8123122,
-                        "hash": HexBytes("3122"),
-                        "parentHash": HexBytes("3121"),
-                        "timestamp": 10002
-                    }),
-                8123123:
-                    AttributeDict({
-                        "number": 8123123,
-                        "hash": HexBytes("3123"),
-                        "parentHash": HexBytes("3122"),
-                        "timestamp": 10003
-                    }),
-            }
-            if arg[0] == "latest":
-                # When "latest" is called the third time, raise an exception so we can test results
-                nonlocal latest_call_counter
-                latest_call_counter += 1
-                if latest_call_counter >= 2:
-                    raise KeyboardInterrupt(
-                        "stop loop for test assertion purposes")
-                return blocks["latest"]
-            else:
-                return blocks[arg[0]]
+    def getBlock_return_values(*arg, **kwargs):
+      blocks = {
+          "latest":
+              AttributeDict({
+                  "number": 8123123,
+                  "hash": HexBytes("3123"),
+                  "parentHash": HexBytes("3122"),
+                  "timestamp": 10003
+              }),
+          8123121:
+              AttributeDict({
+                  "number": 8123121,
+                  "hash": HexBytes("3121"),
+                  "parentHash": HexBytes("3120"),
+                  "timestamp": 10001
+              }),
+          8123122:
+              AttributeDict({
+                  "number": 8123122,
+                  "hash": HexBytes("3122"),
+                  "parentHash": HexBytes("3121"),
+                  "timestamp": 10002
+              }),
+          8123123:
+              AttributeDict({
+                  "number": 8123123,
+                  "hash": HexBytes("3123"),
+                  "parentHash": HexBytes("3122"),
+                  "timestamp": 10003
+              }),
+      }
+      if arg[0] == "latest":
+        # When "latest" is called the third time, raise an exception so we can test results
+        nonlocal latest_call_counter
+        latest_call_counter += 1
+        if latest_call_counter >= 2:
+          raise KeyboardInterrupt("stop loop for test assertion purposes")
+        return blocks["latest"]
+      else:
+        return blocks[arg[0]]
 
-        mock_get_block.side_effect = getBlock_return_values
-        fpb.W3.eth.getBlock = mock_get_block
+    mock_get_block.side_effect = getBlock_return_values
+    fpb.W3.eth.getBlock = mock_get_block
 
-        # Mock requests.get to control responses and inspect GET calls made to the gateway
-        def request_get_handler(*args, **kwargs):
-            response = Mock()
-            response.raise_for_status = lambda: True
-            response.status_code = 200
-            data = {
-                fpb.BENEATH_GET_LATEST_BLOCK_URL: {
-                    "number": 8123123,
-                    "hash": "0x3123",
-                    "parentHash": "0x3122",
-                    "timestamp": 10003
-                },
-                f"{fpb.BENEATH_STREAM_URL}/details": {
-                    "current_instance_id": current_instance_id
-                }
-            }
-            response.json.return_value = data[args[0]]
-            return response
+    # Mock requests.get to control responses and inspect GET calls made to the gateway
+    def request_get_handler(*args, **kwargs):
+      response = Mock()
+      response.raise_for_status = lambda: True
+      response.status_code = 200
+      data = {
+          fpb.BENEATH_GET_LATEST_BLOCK_URL: {
+              "number": 8123123,
+              "hash": "0x3123",
+              "parentHash": "0x3122",
+              "timestamp": 10003
+          },
+          f"{fpb.BENEATH_STREAM_URL}/details": {
+              "current_instance_id": current_instance_id
+          }
+      }
+      response.json.return_value = data[args[0]]
+      return response
 
-        fpb.requests.get = Mock(side_effect=request_get_handler)
+    fpb.requests.get = Mock(side_effect=request_get_handler)
 
-        # Mock requests.post to control behaviour and inspect calls made to it
-        fpb.requests.post = Mock(return_value=AttributeDict({
-            "status_code": 200,
-            "raise_for_status": lambda: True
-        }))
+    # Mock requests.post to control behaviour and inspect calls made to it
+    fpb.requests.post = Mock(return_value=AttributeDict({
+        "status_code": 200,
+        "raise_for_status": lambda: True
+    }))
 
-        # Run the main loop until the test-exception stops it
-        try:
-            fpb.main()
-        except KeyboardInterrupt as ex:
-            print(str(ex))
+    # Run the main loop until the test-exception stops it
+    try:
+      fpb.main()
+    except KeyboardInterrupt as ex:
+      print(str(ex))
 
-        # Assert that W3.eth.getBlock(...) was called exactly as we expected
-        fpb.W3.eth.getBlock.assert_has_calls([
-            call("latest"),
-            call(8123121),
-            call(8123122),
-            call(8123123),
-            call("latest")
-        ],
-                                             any_order=False)
-        self.assertEqual(fpb.W3.eth.getBlock.call_count, 5,
-                         "getBlock must have been called exactly 5 times")
+    # Assert that W3.eth.getBlock(...) was called exactly as we expected
+    fpb.W3.eth.getBlock.assert_has_calls([
+        call("latest"),
+        call(8123121),
+        call(8123122),
+        call(8123123),
+        call("latest")
+    ],
+                                         any_order=False)
+    self.assertEqual(fpb.W3.eth.getBlock.call_count, 5,
+                     "getBlock must have been called exactly 5 times")
 
-        # Assert that the gateway was asked for the latest synced block
-        fpb.get_start_block_no.assert_called_once()
+    # Assert that the gateway was asked for the latest synced block
+    fpb.get_start_block_no.assert_called_once()
 
-        # Assert that blocks where POST'ed to the gateway as we expected
-        expected_post_headers = {
-            "Authorization": f"Bearer {fpb.config.BENEATH_PROJECT_KEY}",
-            "content-type": "application/json"
-        }
-        fpb.requests.post.assert_has_calls([
-            call(fpb.get_beneath_instance_url(current_instance_id),
-                 headers=expected_post_headers,
-                 json={
-                     "@meta": {
-                         "sequence_number": current_time
-                     },
-                     "number": 8123121,
-                     "hash": "0x3121",
-                     "parentHash": "0x3120",
-                     "timestamp": 10001
-                 }),
-            call(fpb.get_beneath_instance_url(current_instance_id),
-                 headers=expected_post_headers,
-                 json={
-                     "@meta": {
-                         "sequence_number": current_time
-                     },
-                     "number": 8123122,
-                     "hash": "0x3122",
-                     "parentHash": "0x3121",
-                     "timestamp": 10002
-                 }),
-            call(fpb.get_beneath_instance_url(current_instance_id),
-                 headers=expected_post_headers,
-                 json={
-                     "@meta": {
-                         "sequence_number": current_time
-                     },
-                     "number": 8123123,
-                     "hash": "0x3123",
-                     "parentHash": "0x3122",
-                     "timestamp": 10003
-                 })
-        ],
-                                           any_order=False)
-        self.assertEqual(fpb.requests.post.call_count, 3,
-                         "requests.post must have been called exactly 3 times")
+    # Assert that blocks where POST'ed to the gateway as we expected
+    expected_post_headers = {
+        "Authorization": f"Bearer {fpb.config.BENEATH_PROJECT_KEY}",
+        "content-type": "application/json"
+    }
+    fpb.requests.post.assert_has_calls([
+        call(fpb.get_beneath_instance_url(current_instance_id),
+             headers=expected_post_headers,
+             json={
+                 "@meta": {
+                     "sequence_number": current_time
+                 },
+                 "number": 8123121,
+                 "hash": "0x3121",
+                 "parentHash": "0x3120",
+                 "timestamp": 10001
+             }),
+        call(fpb.get_beneath_instance_url(current_instance_id),
+             headers=expected_post_headers,
+             json={
+                 "@meta": {
+                     "sequence_number": current_time
+                 },
+                 "number": 8123122,
+                 "hash": "0x3122",
+                 "parentHash": "0x3121",
+                 "timestamp": 10002
+             }),
+        call(fpb.get_beneath_instance_url(current_instance_id),
+             headers=expected_post_headers,
+             json={
+                 "@meta": {
+                     "sequence_number": current_time
+                 },
+                 "number": 8123123,
+                 "hash": "0x3123",
+                 "parentHash": "0x3122",
+                 "timestamp": 10003
+             })
+    ],
+                                       any_order=False)
+    self.assertEqual(fpb.requests.post.call_count, 3,
+                     "requests.post must have been called exactly 3 times")
 
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
