eaf1393724660aadec4683afb0d5ef6f0fd0a236
Eric Green <ericpgreen2@gmail.com>
2019-07-23 16:38:44 +0200

    writing to big table via engine abstraction

--- beneath-go/pipeline/pipeline.go
+++ beneath-go/pipeline/pipeline.go
@@ -154,8 +154,6 @@ func Run() error {
 	})
 }
 
-//
-
 // create data for testing
 // create a uuid
 // uuidTest, _ := uuid.FromString("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
@@ -189,152 +187,15 @@ func Run() error {
 // 	},
 // }
 
-// // consume data from PubSub; unpack the protocol buffer packet
-// // consumeFromPubSub()
-
-// // create a BigTable table called "main" (if it doesn't already exist)
-// createBigTableMain(project, uuidTestBytes)
-
-// // loop through records in packet and write to BigTable and BigQuery
-// // do I want to open/close the client every time within writeToBigTable, or open it once outside the loop
-// for _, record := range testPacket.Records {
-// 	writeToBigTable(project, testPacket.InstanceId, record)
-// 	// writeToBigQuery()
-// }
-
-// // inspect and validate BigTable records
-// readFromBigTable(project, testPacket.InstanceId)
-
-// func createBigTableMain(project string, instanceID []byte) {
-// 	// for testing:
-// 	// "project" and "instance" parameters in NewClient() are ignored when using the emulator
-// 	// signal that you are using the BigTable emulator by setting the BIGTABLE_EMULATOR_HOST environment variable
-// 	// once the emulator is running, run "$(gcloud beta emulators bigtable env-init)" in a terminal to set the environment variable
-// 	ctx := context.Background()
-// 	adminClient, err := bigtable.NewAdminClient(ctx, project, uuid.FromBytesOrNil(instanceID).String())
-// 	if err != nil {
-// 		log.Fatalf("Could not create admin client: %v", err)
-// 	}
-
-// 	err = adminClient.CreateTable(ctx, "main")
-// 	if err != nil {
-// 		// ignore error if table already exists
-// 		if !strings.Contains(err.Error(), "AlreadyExists") {
-// 			log.Fatalf("Could not create table: %v", err)
-// 		}
-// 	}
-
-// 	err = adminClient.CreateColumnFamily(ctx, "main", "columnfamily0")
-// 	if err != nil {
-// 		// ignore error if column family already exists
-// 		if !strings.Contains(err.Error(), "AlreadyExists") {
-// 			log.Fatalf("Could not create column family: %v", err)
-// 		}
-// 	}
-// }
-
-// func writeToBigTable(project string, instanceID []byte, record *pb.Record) {
-
-// 	ctx := context.Background()
-
-// 	client, err := bigtable.NewClient(ctx, project, uuid.FromBytesOrNil(instanceID).String())
-// 	if err != nil {
-// 		log.Fatalf("Could not create data operations client: %v", err)
-// 	}
-
-// 	tbl := client.Open("main")
-
 // 	// use this random recordkey for testing
 // 	RecordKey := make([]byte, 10)
 // 	rand.Read(RecordKey)
 
-// 	rowKeys := append(instanceID, RecordKey...)
-// 	columnFamilyName := "columnfamily0"
-// 	column0Name := "data"
-// 	// column1Name := "sequenceNumber"
-
-// 	log.Printf("writing to table.")
-// 	muts := bigtable.NewMutation()
-// 	muts.Set(columnFamilyName, column0Name, bigtable.Timestamp(record.SequenceNumber), record.AvroData)
-// 	//muts.Set(columnFamilyName, column1Name, bigtable.Now(), []byte(record.SequenceNumber))
-
-// 	err = tbl.Apply(ctx, string(rowKeys), muts)
-// 	if err != nil {
-// 		log.Fatalf("Could not apply row mutation: %v", err)
-// 	}
-
-// 	if err = client.Close(); err != nil {
-// 		log.Fatalf("Could not close data operations client: %v", err)
-// 	}
-// }
-
-// func readFromBigTable(project string, instanceID []byte) {
-
-// 	ctx := context.Background()
-
-// 	client, err := bigtable.NewClient(ctx, project, uuid.FromBytesOrNil(instanceID).String())
-// 	if err != nil {
-// 		log.Fatalf("Could not create data operations client: %v", err)
-// 	}
-
-// 	tbl := client.Open("main")
-
-// 	log.Printf("Reading all rows:")
-// 	rr := bigtable.PrefixRange("")
-// 	err = tbl.ReadRows(ctx, rr, func(row bigtable.Row) bool {
-// 		log.Print(row["columnfamily0"][0].Row)
-// 		log.Print(row["columnfamily0"][0].Column)
-// 		log.Print(row["columnfamily0"][0].Timestamp)
-// 		log.Print(row["columnfamily0"][0].Value)
-// 		return true
-// 	})
-
-// 	if err = client.Close(); err != nil {
-// 		log.Fatalf("Could not close data operations client: %v", err)
-// 	}
-// }
-
 /*  Notes, questions, and extra code
 
 write both successful rows and failed rows to BigQuery
-will the bigquery table already exist? or will I sometimes have to create it?
 use kubernetes to auto-scale machines in pipeline if needed (this is a beam replacement)
 get schema from benjamin's go helper functions
-
-func consumeFromPubSub() {
-
-	// set environment variables
-	os.Setenv("PUBSUB_EMULATOR_HOST", "localhost:8085")
-	os.Setenv("PUBSUB_PROJECT_ID", "beneathcrypto")
-
-	// prepare pubsub client
-	client, err := pubsub.NewClient(context.Background(), "beneathcrypto")
-	if err != nil {
-		log.Fatalf("could not create pubsub client: %v", err)
-	}
-
-	// create subscriber to topic
-	sub, err := client.CreateSubscription(context.Background(), "mySubscriptionName3", pubsub.SubscriptionConfig{
-		Topic:       client.Topic("test-topic"),
-		AckDeadline: 20 * time.Second,
-	})
-	if err != nil {
-		log.Printf(err.Error())
-		log.Printf("could not subscribe to topic")
-	} else {
-		log.Printf("Created subscription: %v\n", sub)
-	}
-
-	// consume messages
-	ctx := context.Background()
-	err = sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
-		msg.Ack()
-		fmt.Printf("Got message: %q\n", string(msg.Data))
-	})
-	if err != nil {
-		log.Fatalf("Error!")
-	}
-}
 
 func writeToBigQuery() {
 
