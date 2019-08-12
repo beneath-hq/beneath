1a3f10746d72031ece412e3d533f49b430c958e4
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-08-12 17:26:04 +0200

    updated query

--- beneath-models-makerdao/dai-transfers/query.sql
+++ beneath-models-makerdao/dai-transfers/query.sql
@@ -8,13 +8,13 @@ with logs as (
   select l.transaction_hash as transaction_hash, l.block_timestamp as time, l.log_index as index_in_block, l.topics, l.data
   from `bigquery-public-data.crypto_ethereum.logs` l
   where l.address = '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359'  
-  and l.block_timestamp > timestamp(date(2019, 7, 11))
+  and l.block_timestamp > timestamp(date(2019, 8, 1))
 )
 select
   *
 from (
   select
-    l.transaction_hash,
+    l.transaction_hash as `transactionHash`,
     unix_millis(l.time) as `time`,
     l.index_in_block as `index`,
     'Transfer' as `name`,
@@ -25,7 +25,7 @@ from (
   where l.topics[offset(0)] = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- Transfer
 ) union all (
   select
-    l.transaction_hash,
+    l.transaction_hash as `transactionHash`,
     unix_millis(l.time) as `time`,
     l.index_in_block as `index`,
     'Mint' as `name`,
@@ -36,7 +36,7 @@ from (
   where l.topics[offset(0)] = '0x0f6798a560793a54c3bcfe86a93cde1e73087d944c0ea20544137d4121396885' -- Mint
 ) union all (
   select
-    l.transaction_hash,
+    l.transaction_hash as `transactionHash`,
     unix_millis(l.time) as `time`,
     l.index_in_block as `index`,
     'Burn' as `name`,
