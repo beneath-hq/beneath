37dc77fde40a532198e7419dd00caca74e290f3b
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-08-13 18:17:38 +0200

    formatting data in tables

--- beneath-frontend/components/stream/schema.tsx
+++ beneath-frontend/components/stream/schema.tsx
@@ -1,5 +1,6 @@
 import avro from "avsc";
 import _ from "lodash";
+import numeral from "numeral";
 
 import TableCell from "@material-ui/core/TableCell";
 import Moment from "react-moment";
@@ -16,7 +17,12 @@ export class Schema {
 
   constructor(stream: QueryStream_stream, includeTimestamp: boolean) {
     this.keyFields = stream.keyFields;
-    this.avroSchema = avro.Type.forSchema(JSON.parse(stream.avroSchema)) as avro.types.RecordType;
+    this.avroSchema = avro.Type.forSchema(JSON.parse(stream.avroSchema), {
+      logicalTypes: {
+        decimal: Decimal,
+        "timestamp-millis": DateType,
+      },
+    }) as avro.types.RecordType;
     this.columns = [];
     this.includeTimestamp = includeTimestamp;
     for (const field of this.avroSchema.fields) {
@@ -80,13 +86,52 @@ class Column {
         return <Moment fromNow ago date={new Date(val)} />;
       }
 
-      if (this.name === "day" || this.name === "time") {
-        // TODODODO
+      if (avro.Type.isType(this.type, "logical:timestamp-millis")) {
         return new Date(val).toISOString().slice(0, 19);
+      }
+
+      if (avro.Type.isType(this.type, "int", "long")) {
+        return numeral(val).format("0,0");
+      }
+
+      if (avro.Type.isType(this.type, "float", "double")) {
+        return numeral(val).format("0,0.000");
+      }
+
+      if (avro.Type.isType(this.type, "logical:decimal")) {
+        return numeral(val).format("0,0");
       }
 
       return val.toString();
     }
     return "";
+  }
+}
+
+class DateType extends avro.types.LogicalType {
+  public _fromValue(val: any) {
+    return new Date(val);
+  }
+  public _toValue(date: any) {
+    return date instanceof Date ? +date : undefined;
+  }
+  public _resolve(type: avro.Type) {
+    if (avro.Type.isType(type, "long", "string", "logical:timestamp-millis")) {
+      return this._fromValue;
+    }
+  }
+}
+
+class Decimal extends avro.types.LogicalType {
+  public _fromValue(val: any) {
+    return val;
+  }
+  public _toValue(val: any) {
+    return val;
+  }
+  public _resolve(type: avro.Type) {
+    if (avro.Type.isType(type, "long", "string", "logical:decimal")) {
+      return this._fromValue;
+    }
   }
 }
