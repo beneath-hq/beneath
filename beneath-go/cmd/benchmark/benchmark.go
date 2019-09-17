afa13f055a541c0890f8f2c25e8333660c0874ef
Eric Green <ericpgreen2@gmail.com>
2019-09-17 17:19:21 +0200

    Gateway: added cache for usage metrics

---
+++ beneath-go/cmd/benchmark/benchmark.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/beneath-core/beneath-go/control/entity"
+	"github.com/beneath-core/beneath-go/core"
+	"github.com/beneath-core/beneath-go/db"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type configSpecification struct {
+	StreamsDriver   string `envconfig:"ENGINE_STREAMS_DRIVER" required:"true"`
+	TablesDriver    string `envconfig:"ENGINE_TABLES_DRIVER" required:"true"`
+	WarehouseDriver string `envconfig:"ENGINE_WAREHOUSE_DRIVER" required:"true"`
+	RedisURL        string `envconfig:"CONTROL_REDIS_URL" required:"true"`
+	PostgresURL     string `envconfig:"CONTROL_POSTGRES_URL" required:"true"`
+}
+
+var (
+	// Config ...
+	Config configSpecification
+)
+
+func init() {
+	core.LoadConfig("beneath", &Config)
+	db.InitPostgres(Config.PostgresURL)
+	db.InitRedis(Config.RedisURL)
+	db.InitEngine(Config.StreamsDriver, Config.TablesDriver, Config.WarehouseDriver)
+}
+
+func main() {
+	group := new(errgroup.Group)
+
+	user, err := entity.CreateOrUpdateUser(context.Background(), "abc", "", "test@example.com", "Mr. Test", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n := 500
+	m := 10
+	for i := 0; i < n; i++ {
+		group.Go(func() error {
+			k := i
+
+			secret, err := entity.CreateUserSecret(context.Background(), user.UserID, entity.SecretRoleReadWrite, "")
+			if err != nil {
+				return err
+			}
+
+			client := &http.Client{
+				Timeout: 5 * time.Second,
+			}
+
+			for j := 0; j < m; j++ {
+				req, err := http.NewRequest("GET", "http://localhost:5000/streams/instances/eacdd317-c2c5-4287-af44-4ab58ffc2166", nil)
+				if err != nil {
+					return err
+				}
+				req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", secret.SecretString))
+				res, err := client.Do(req)
+				if err != nil {
+					return err
+				}
+
+				res.Body.Close()
+
+				if k%50 == 0 {
+					if j%10 == 0 {
+						log.Printf("%d -- %d\n", i, j)
+					}
+				}
+			}
+			return nil
+		})
+	}
+
+	// run simultaneously
+	log.Println(group.Wait())
+}
