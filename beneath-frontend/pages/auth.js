de6a3cc8ca1a98e8ba46b9112f939ce53d1dd90b
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-09-30 19:50:17 +0200

    slightly better auth screen

--- beneath-frontend/pages/auth.js
+++ beneath-frontend/pages/auth.js
@@ -7,40 +7,67 @@ import { withStyles } from "@material-ui/styles";
 import connection from "../lib/connection";
 import Page from "../components/Page";
 import { GoogleIcon, GithubIcon } from "../components/Icons";
+import VSpace from "../components/VSpace";
 
 const styles = (theme) => ({
   authButtons: {
     marginTop: theme.spacing(4),
   },
+  authButton: {
+    width: "100%",
+  },
   icon: {
     fontSize: 24,
     marginRight: theme.spacing(1),
   },
+  title: {
+    lineHeight: "150%",
+    marginBottom: theme.spacing(2),
+  },
 });
 
 export default withStyles(styles)(({ classes }) => (
-  <Page title="Sign Up or Log In" contentMarginTop="normal">
+  <Page title="Register or Login" contentMarginTop="normal">
     <div>
-      <Container maxWidth="lg">
-        <Typography component="h2" variant="h5" align="center" gutterBottom>
-          Sign Up or Log In
+      <Container maxWidth="xs">
+        <Typography className={classes.title} component="h2" variant="h1" align="center">
+          Hello there! Pick an option to register or login
         </Typography>
         <div className={classes.authButtons}>
           <Grid container spacing={2} justify="center">
-            <Grid item>
-              <Button size="large" color="primary" variant="outlined" href={`${connection.API_URL}/auth/github`}>
+            <Grid item xs={12}>
+              <Button
+                className={classes.authButton}
+                size="large"
+                color="primary"
+                variant="outlined"
+                href={`${connection.API_URL}/auth/github`}
+              >
                 <GithubIcon className={classes.icon} />
                 Connect with Github
               </Button>
             </Grid>
-            <Grid item>
-              <Button size="large" color="primary" variant="outlined" href={`${connection.API_URL}/auth/google`}>
+            <Grid item xs={12}>
+              <Button
+                className={classes.authButton}
+                size="large"
+                color="primary"
+                variant="outlined"
+                href={`${connection.API_URL}/auth/google`}
+              >
                 <GoogleIcon className={classes.icon} />
                 Connect with Google
               </Button>
             </Grid>
           </Grid>
         </div>
+        <VSpace units={4} />
+        <Typography className={classes.title} variant="body2" color={"textSecondary"} align="center">
+          * We promise to treat your personal details with care
+        </Typography>
+        <Typography className={classes.title} variant="body2" color={"textSecondary"} align="center">
+          ** Manual user authentication coming soon
+        </Typography>
       </Container>
     </div>
   </Page>
