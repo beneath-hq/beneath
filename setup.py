440a4ab1b3ef609ce3a298695f316f4f7e88fea0
Benjamin Egelund-Müller <benjamin@beneath.network>
2019-10-01 12:06:30 +0200

    neater setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,38 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# Get long_description
+with open('README.md', 'r') as f:
+  LONG_DESCRIPTION = f.read()
 
-# TODO: input correct specifications
+# Get version
+with open('beneath/_version.py', 'r') as f:
+  for line in f:
+    if line.startswith('__version__'):
+      _, _, VERSION = line.replace('"', '').split()
+      break
+
 setuptools.setup(
-  name="beneath-python-package",
-  version=open(
-    "beneath/_version.py").readlines()[-1].split()[-1].strip("\"'"),
-  author="Benjamin Egelund-Muller and Eric Green",
-  author_email="eric@beneath.network",
-  description="Client package for Beneath Systems",
-  long_description=long_description,
-  long_description_content_type="text/markdown",
+  name="beneath-python",
+  version=VERSION,
+  author="Beneath Systems",
+  author_email="hello@beneath.network",
+  description="Python client for Beneath (https://beneath.network/)",
   url="https://beneath.network/",
+  long_description=LONG_DESCRIPTION,
+  long_description_content_type='text/markdown',
   packages=setuptools.find_packages(),
   classifiers=[
     "Programming Language :: Python :: 3",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
   ],
-  scripts=['bin/beneathcmd'],
+  scripts=['bin/beneath'],
   install_requires=[
-    'apache_beam',
-    'argparse',
-    'fastavro',
-    'grpcio-tools',
-    'pandas'
+    'apache_beam[gpc]>=2.14.0',
+    'argparse>=1.1',
+    'fastavro>=0.21.24',
+    'grpcio>=1.23.0',
+    'pandas>=0.24.2',
+    'six>=1.12.0',
   ]
 )
