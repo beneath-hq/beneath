317e637133ca8e59c069c9ba5990492864f5ea3d
Benjamin Egelund-Müller <benjamin@beneath.network>
2019-10-07 12:04:56 +0200

    "no content here yet" labels

---
+++ beneath-frontend/components/project/ViewStreams.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from "react";
+
+import { List, ListItem, ListItemAvatar, ListItemText, makeStyles, Typography } from "@material-ui/core";
+
+import { ProjectByName_projectByName } from "../../apollo/types/ProjectByName";
+import { toURLName } from "../../lib/names";
+import Avatar from "../Avatar";
+import NextMuiLinkList from "../NextMuiLinkList";
+
+interface Props {
+  project: ProjectByName_projectByName;
+}
+
+const useStyles = makeStyles((theme) => ({
+  noDataCaption: {
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const ViewStreams: FC<Props> = ({ project }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <List>
+        {project.streams.map(({ streamID, name, description, external }) => (
+          <ListItem
+            key={streamID}
+            component={NextMuiLinkList}
+            as={`/projects/${toURLName(project.name)}/streams/${toURLName(name)}`}
+            href={`/stream?name=${toURLName(name)}&project_name=${toURLName(project.name)}`}
+            button
+            disableGutters
+          >
+            <ListItemAvatar>
+              <Avatar size="list" label={external ? "Root" : "Derived"} />
+            </ListItemAvatar>
+            <ListItemText primary={toURLName(name)} secondary={description} />
+          </ListItem>
+        ))}
+      </List>
+      {project.streams.length === 0 && (
+        <Typography className={classes.noDataCaption} variant="body1" align="center">
+          There are no streams in this project
+        </Typography>
+      )}
+    </>
+  );
+};
+
+export default ViewStreams;
---
+++ beneath-frontend/components/user/ViewProjects.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from "react";
+
+import { List, ListItem, ListItemAvatar, ListItemText, makeStyles, Typography } from "@material-ui/core";
+
+import { User_user } from "../../apollo/types/User";
+import { toURLName } from "../../lib/names";
+import Avatar from "../Avatar";
+import NextMuiLinkList from "../NextMuiLinkList";
+
+const useStyles = makeStyles((theme) => ({
+  noDataCaption: {
+    color: theme.palette.text.secondary,
+  },
+}));
+
+interface Props {
+  user: User_user;
+}
+
+const ViewProjects: FC<Props> = ({ user }) => {
+  const classes = useStyles();
+  return (
+    <>
+      <List>
+        {user.projects.map(({ projectID, name, displayName, description, photoURL }) => (
+          <ListItem
+            component={NextMuiLinkList}
+            href={`/project?name=${toURLName(name)}`}
+            button
+            disableGutters
+            key={projectID}
+            as={`/projects/${toURLName(name)}`}
+          >
+            <ListItemAvatar>
+              <Avatar size="list" label={displayName || name} src={photoURL || undefined} />
+            </ListItemAvatar>
+            <ListItemText primary={displayName || name} secondary={description} />
+          </ListItem>
+        ))}
+      </List>
+      {user.projects.length === 0 && (
+        <Typography className={classes.noDataCaption} variant="body1" align="center">
+          @{user.username} isn't a member of any projects
+        </Typography>
+      )}
+    </>
+  );
+};
+
+export default ViewProjects;
