1e5a7a68414a3a0421b4fb6efd9cd48f8ed38fda
Benjamin Egelund-Müller <benjamin@beneath.network>
2019-10-28 10:27:57 +0100

    fix kebab case <-> underscore case bug

--- beneath-frontend/lib/names.tsx
+++ beneath-frontend/lib/names.tsx
@@ -1,7 +1,7 @@
 export const toURLName = (name: string) => {
-  return name.replace("_", "-");
+  return name.replace(/_/g, "-");
 };
 
 export const toBackendName = (name: string) => {
-  return name.replace("-", "_");
+  return name.replace(/-/g, "_");
 };
