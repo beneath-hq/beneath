0ca8a79a9c996b42ac573cfd11c28ac8324c43fd
Eric Green <ericpgreen2@gmail.com>
2019-11-12 14:43:59 +0100

    updated migrations

--- beneath-go/control/migrations/014_billing.go
+++ beneath-go/control/migrations/014_billing.go
@@ -20,12 +20,21 @@ func init() {
 			return err
 		}
 
-		// Organization.Personal, Organization.BillingPlanID, Organization.StripeCustomerID
+		// (billing_time, org_id, entity_id, product) unique index
+		_, err = db.Exec(`
+			CREATE UNIQUE INDEX billed_resources_billing_time_organization_id_entity_id_product_key ON public.billed_resources USING btree (billing_time, organization_id, entity_id, product);
+		`)
+		if err != nil {
+			return err
+		}
+
+		// Organization.Personal, Organization.BillingPlanID, Organization.StripeCustomerID, Organization.PaymentMethod
 		_, err = db.Exec(`
 			ALTER TABLE organizations
 			ADD personal bool NOT NULL default FALSE,
 			ADD billing_plan_id uuid,
-			ADD stripe_customer_id varchar;
+			ADD stripe_customer_id varchar,
+			ADD payment_method varchar;
 		`)
 		if err != nil {
 			return err
@@ -46,11 +55,12 @@ func init() {
 			return err
 		}
 
-		// Organization.Personal, Organization.BillingPlanID, Organization.StripeCustomerID
+		// Organization.Personal, Organization.BillingPlanID, Organization.StripeCustomerID, Organization.PaymentMethod
 		_, err = db.Exec(`
 			ALTER TABLE organizations DROP personal;
 			ALTER TABLE organizations DROP billing_plan_id;
 			ALTER TABLE organizations DROP stripe_customer_id;
+			ALTER TABLE organizations DROP payment_method;
 		`)
 		if err != nil {
 			return err
