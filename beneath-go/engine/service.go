13fffcb7d83fbc4ecab16428b103aeccf1e6aaeb
Benjamin Egelund-Müller <b@egelund-muller.com>
2019-12-18 16:07:46 +0100

    implemented service functions on engine

--- beneath-go/engine/service.go
+++ beneath-go/engine/service.go
@@ -3,37 +3,102 @@ package engine
 import (
 	"context"
 
+	"golang.org/x/sync/errgroup"
+
 	"github.com/beneath-core/beneath-go/engine/driver"
 )
 
 // RegisterProject is called when a project is created *or updated*
 func (e *Engine) RegisterProject(ctx context.Context, p driver.Project) error {
-	panic("todo")
+	group, ctx := errgroup.WithContext(ctx)
+
+	group.Go(func() error {
+		return e.Log.RegisterProject(ctx, p)
+	})
+
+	group.Go(func() error {
+		return e.Lookup.RegisterProject(ctx, p)
+	})
+
+	group.Go(func() error {
+		return e.Warehouse.RegisterProject(ctx, p)
+	})
+
+	return group.Wait()
 }
 
 // RemoveProject is called when a project is deleted
 func (e *Engine) RemoveProject(ctx context.Context, p driver.Project) error {
-	panic("todo")
+	group, ctx := errgroup.WithContext(ctx)
+
+	group.Go(func() error {
+		return e.Log.RemoveProject(ctx, p)
+	})
+
+	group.Go(func() error {
+		return e.Lookup.RemoveProject(ctx, p)
+	})
+
+	group.Go(func() error {
+		return e.Warehouse.RemoveProject(ctx, p)
+	})
+
+	return group.Wait()
 }
 
 // RegisterInstance is called when a new instance is created
 func (e *Engine) RegisterInstance(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance) error {
-	panic("todo")
+	group, ctx := errgroup.WithContext(ctx)
+
+	group.Go(func() error {
+		return e.Log.RegisterInstance(ctx, p, s, i)
+	})
+
+	group.Go(func() error {
+		return e.Lookup.RegisterInstance(ctx, p, s, i)
+	})
+
+	group.Go(func() error {
+		return e.Warehouse.RegisterInstance(ctx, p, s, i)
+	})
+
+	return group.Wait()
 }
 
 // PromoteInstance is called when an instance is promoted to be the main instance of the stream
 func (e *Engine) PromoteInstance(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance) error {
-	panic("todo")
+	group, ctx := errgroup.WithContext(ctx)
+
+	group.Go(func() error {
+		return e.Log.PromoteInstance(ctx, p, s, i)
+	})
+
+	group.Go(func() error {
+		return e.Lookup.PromoteInstance(ctx, p, s, i)
+	})
+
+	group.Go(func() error {
+		return e.Warehouse.PromoteInstance(ctx, p, s, i)
+	})
+
+	return group.Wait()
 }
 
 // RemoveInstance is called when an instance is deleted
 func (e *Engine) RemoveInstance(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance) error {
-	panic("todo")
+	group, ctx := errgroup.WithContext(ctx)
 
-	// TODO:
-	// delete in bigtable records
-	// err = db.Engine.Tables.ClearRecords(ctx, t.InstanceID)
-	// if err != nil {
-	// 	return err
-	// }
+	group.Go(func() error {
+		return e.Log.RemoveInstance(ctx, p, s, i)
+	})
+
+	group.Go(func() error {
+		return e.Lookup.RemoveInstance(ctx, p, s, i)
+	})
+
+	group.Go(func() error {
+		return e.Warehouse.RemoveInstance(ctx, p, s, i)
+	})
+
+	return group.Wait()
 }
