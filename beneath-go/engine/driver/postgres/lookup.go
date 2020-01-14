18f0fea0260a278bf434d176ed23d73a6673ab33
Benjamin Egelund-Müller <b@egelund-muller.com>
2020-01-14 18:41:27 +0100

    stubs for postgres driver in engine

---
+++ beneath-go/engine/driver/postgres/lookup.go
@@ -0,0 +1,34 @@
+package postgres
+
+import (
+	"context"
+
+	"github.com/beneath-core/beneath-go/core/queryparse"
+	"github.com/beneath-core/beneath-go/engine/driver"
+)
+
+// ParseQuery implements driver.LookupService
+func (b Postgres) ParseQuery(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance, where queryparse.Query, compacted bool, partitions int) ([][]byte, [][]byte, error) {
+	panic("todo")
+}
+
+// ReadCursor implements driver.LookupService
+func (b Postgres) ReadCursor(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance, cursor []byte, limit int) (driver.RecordsIterator, error) {
+	panic("todo")
+}
+
+// WriteRecords implements driver.LookupService
+func (b Postgres) WriteRecords(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance, rs []driver.Record) error {
+	// postgres: s.compacted
+	// - indexes for secondary, primary key for key, index for offset
+	// - upsert on primary key with new offset (autoincrement) conditional on timestamp greater
+	// - secondary indexes fix themselves
+
+	// postgres: !s.compacted
+	// - primary key on offset
+	// - current = true|false
+	// - index on (key/secondary, current=true)
+	// - set current to false on old when insert new (if timesamp greater)
+
+	panic("todo")
+}
---
+++ beneath-go/engine/driver/postgres/warehouse.go
@@ -0,0 +1,12 @@
+package postgres
+
+import (
+	"context"
+
+	"github.com/beneath-core/beneath-go/engine/driver"
+)
+
+// WriteToWarehouse implements beneath.WarehouseService
+func (b Postgres) WriteToWarehouse(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance, rs []driver.Record) error {
+	panic("todo")
+}
