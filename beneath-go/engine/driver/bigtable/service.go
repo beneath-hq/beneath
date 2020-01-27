4ded27a72dc89e894feaf8b80c5cfa0821659e97
Benjamin Egelund-Müller <b@egelund-muller.com>
2020-01-27 11:30:36 +0100

    cleanup logic

--- beneath-go/engine/driver/bigtable/service.go
+++ beneath-go/engine/driver/bigtable/service.go
@@ -8,40 +8,87 @@ import (
 
 // MaxKeySize implements beneath.Service
 func (b BigTable) MaxKeySize() int {
-	panic("todo")
+	// Bigtable limit is 4096 bytes
+	// Longest keys are instanceID#secondaryKey#primaryKey
+	return 1024 // 1 kb
 }
 
 // MaxRecordSize implements beneath.Service
 func (b BigTable) MaxRecordSize() int {
-	panic("todo")
+	// Bigtable goes up to 10 MB, but performance suffers
+	// We have to be careful to support user requests of 1000 rows at once
+	return 8192 // 8 kb
 }
 
 // MaxRecordsInBatch implements beneath.Service
 func (b BigTable) MaxRecordsInBatch() int {
-	panic("todo")
+	return 10000
 }
 
 // RegisterProject implements beneath.Service
 func (b BigTable) RegisterProject(ctx context.Context, p driver.Project) error {
-	panic("todo")
+	return nil
 }
 
 // RemoveProject implements beneath.Service
 func (b BigTable) RemoveProject(ctx context.Context, p driver.Project) error {
-	panic("todo")
+	return nil
 }
 
 // RegisterInstance implements beneath.Service
 func (b BigTable) RegisterInstance(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance) error {
-	panic("todo")
+	return nil
 }
 
 // PromoteInstance implements beneath.Service
 func (b BigTable) PromoteInstance(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance) error {
-	panic("todo")
+	return nil
 }
 
 // RemoveInstance implements beneath.Service
 func (b BigTable) RemoveInstance(ctx context.Context, p driver.Project, s driver.Stream, i driver.StreamInstance) error {
-	panic("todo")
+	codec := s.GetCodec()
+
+	// get table names
+	logTable, indexesTable := logTableName, indexesTableName
+	if streamExpires(s) {
+		logTable, indexesTable = logExpiringTableName, indexesExpiringTableName
+	}
+
+	// secondary indexes
+	for _, index := range codec.SecondaryIndexes {
+		indexID := index.GetIndexID()
+		err := b.Admin.DropRowRange(ctx, indexesTable, string(indexID[:]))
+		if err != nil {
+			return err
+		}
+	}
+
+	// primary index
+	indexID := codec.PrimaryIndex.GetIndexID()
+	err := b.Admin.DropRowRange(ctx, indexesTable, string(indexID[:]))
+	if err != nil {
+		return err
+	}
+
+	// log
+	instanceID := i.GetStreamInstanceID()
+	err = b.Admin.DropRowRange(ctx, logTable, string(instanceID[:]))
+	if err != nil {
+		return err
+	}
+
+	// sequencer
+	err = b.Sequencer.ClearState(ctx, makeSequencerKey(instanceID))
+	if err != nil {
+		return err
+	}
+
+	// metrics
+	err = b.Admin.DropRowRange(ctx, metricsTableName, string(instanceID[:]))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
