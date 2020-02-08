fda0ed6e03dff9577588bd217b4aea9395a55087
Benjamin Egelund-Müller <b@egelund-muller.com>
2020-02-08 17:10:48 +0100

    implemented metrics proxy funtctions on engine

--- beneath-go/engine/metrics.go
+++ beneath-go/engine/metrics.go
@@ -8,15 +8,15 @@ import (
 
 // CommitUsage writes a batch of usage metrics
 func (e *Engine) CommitUsage(ctx context.Context, key []byte, usage pb.QuotaUsage) error {
-	panic("todo")
+	return e.Lookup.CommitUsage(ctx, key, usage)
 }
 
 // ReadSingleUsage reads usage metrics for one key
 func (e *Engine) ReadSingleUsage(ctx context.Context, key []byte) (pb.QuotaUsage, error) {
-	panic("todo")
+	return e.Lookup.ReadSingleUsage(ctx, key)
 }
 
 // ReadUsage reads usage metrics for multiple periods and calls fn one by one
 func (e *Engine) ReadUsage(ctx context.Context, fromKey []byte, toKey []byte, fn func(key []byte, usage pb.QuotaUsage) error) error {
-	panic("todo")
+	return e.Lookup.ReadUsage(ctx, fromKey, toKey, fn)
 }
