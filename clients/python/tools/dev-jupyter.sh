fe9eefa81364e51f777d6e7ae7c0f1c4fff23db6
Benjamin Egelund-Müller <b@egelund-muller.com>
2020-03-02 18:36:23 +0100

    run jupyter with dev version

---
+++ clients/python/tools/dev-jupyter.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# check we're in the client repo
+if [[ ! -f tools/$(basename $0) ]]; then
+  echo "you must execute this script from the python client root folder (clients/python)"
+  exit 1
+fi
+
+PYTHONPATH=$(pwd $0):$PYTHONPATH jupyter notebook
