513921a7ffdee05d51d7bb8fe0897b27fc82cbe4
Benjamin Egelund-Müller <b@egelund-muller.com>
2020-03-17 17:58:17 +0100

    tslint and prettier

---
+++ clients/js/prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  printWidth: 120,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+};
