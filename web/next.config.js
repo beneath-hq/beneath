ccad43da0c79129e5401128fc3289f336d36cf68
Benjamin Egelund-Müller <b@egelund-muller.com>
2020-03-24 18:10:44 +0100

    added beneath-react

---
+++ web/next.config.js
@@ -0,0 +1,15 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    // development config
+    return {
+      env: {
+        BENEATH_ENV: "development",
+      },
+    };
+  }
+
+  // production config
+  return {};
+};
