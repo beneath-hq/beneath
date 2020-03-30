36e87dbba18f1d6d5a93b57d43a08a0b7a03abf5
greenep12 <ericpgreen2@gmail.com>
2020-03-30 12:05:02 +0200

    springboard mvp

---
+++ web/components/terminal/TopProjects.tsx
@@ -0,0 +1,89 @@
+import React, { FC } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import {
+  Container,
+  Grid,
+  makeStyles,
+  Paper,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import Link from "next/link";
+import { toURLName } from "../../lib/names";
+import Avatar from "../Avatar";
+import { EXPLORE_PROJECTS } from "../../apollo/queries/project";
+import { ExploreProjects } from "../../apollo/types/ExploreProjects";
+import ErrorPage from "../ErrorPage";
+import Loading from "../Loading";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  sectionHeader: {
+    fontSize: theme.typography.pxToRem(24),
+    marginBottom: theme.spacing(6),
+  },
+  sectionSubHeader: {
+    marginTop: theme.spacing(2),
+  },
+  avatar: {
+    marginRight: theme.spacing(1.5),
+    marginBottom: theme.spacing(1.5),
+  },
+  paper: {
+    cursor: "pointer",
+    padding: theme.spacing(2.5),
+    height: "100%",
+    "&:hover": {
+      boxShadow: theme.shadows[10],
+    },
+  },
+}));
+
+const TopProjects: FC = () => {
+  const classes = useStyles();
+
+  const { loading, error, data } = useQuery<ExploreProjects>(EXPLORE_PROJECTS);
+  if (loading) {
+    return <Loading justify="center" />;
+  }
+
+  if (error || !data) {
+    return <ErrorPage apolloError={error} />;
+  }
+
+  return (
+    <React.Fragment>
+      <Container maxWidth="lg">
+        <Typography className={classes.sectionHeader} variant="h3" gutterBottom align="center">
+          Top projects
+                <Typography className={classes.sectionSubHeader} variant="body2" gutterBottom align="center">
+            Discover the top public data streams on Beneath.
+                </Typography>
+        </Typography>
+        <Grid container spacing={3} justify="center">
+          {data.exploreProjects.map(({ projectID, name, displayName, description, photoURL }) => (
+            <Grid key={projectID} item lg={4} md={6} xs={12}>
+              <Link href={`/project?name=${toURLName(name)}`} as={`/projects/${toURLName(name)}`}>
+                <Paper className={classes.paper}>
+                  <Grid container wrap="nowrap" spacing={0}>
+                    <Grid item className={classes.avatar}>
+                      <Avatar size="list" label={displayName || name} src={photoURL || undefined} />
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="h2">{displayName || toURLName(name)}</Typography>
+                      <Typography color="textSecondary" variant="body2" gutterBottom>
+                        /projects/{toURLName(name)}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Typography variant="body1">{description}</Typography>
+                </Paper>
+              </Link>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </React.Fragment>
+  )
+}
+
+export default TopProjects
---
+++ web/components/terminal/Welcome.tsx
@@ -0,0 +1,76 @@
+import React, { FC } from "react";
+import {
+  Button,
+  Container,
+  Grid,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
+import clsx from "clsx";
+import NextMuiLinkList from "../NextMuiLinkList";
+import TopProjects from "../../components/terminal/TopProjects";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  title: {
+    fontSize: theme.typography.pxToRem(52),
+    marginBottom: theme.spacing(5),
+  },
+  subtitle: {
+    fontSize: theme.typography.pxToRem(20),
+    marginBottom: theme.spacing(6),
+  },
+  buttons: {
+    marginBottom: theme.spacing(6),
+  },
+  button: {},
+  primaryButton: {},
+  secondaryButton: {},
+}));
+
+const Welcome: FC = () => {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <Container maxWidth="lg">
+        <Typography className={classes.title} component="h1" variant="h1" align="center" gutterBottom>
+          Welcome to the Beneath Data Terminal
+        </Typography>
+      </Container>
+      <Container maxWidth="md">
+        <Typography className={classes.subtitle} component="h2" variant="subtitle1" align="center" gutterBottom>
+          Discover real-time data streams that have been shared with the world. Consume public data or create your own stream in minutes.
+        </Typography>
+        <Grid className={classes.buttons} container spacing={2} justify="center">
+          <Grid item>
+            <Button
+              size="medium"
+              color="default"
+              variant="outlined"
+              className={clsx(classes.button, classes.secondaryButton)}
+              href={`https://about.beneath.dev/docs/`}
+            >
+              Overview
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              size="medium"
+              color="primary"
+              variant="outlined"
+              className={clsx(classes.button, classes.primaryButton)}
+              href={`/auth`}
+              component={NextMuiLinkList}
+            >
+              Create Account
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+      <TopProjects/>
+    </React.Fragment>
+  )
+}
+
+export default Welcome;
