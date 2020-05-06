e9521f9d441598f371a0ebe33acf852e7f8013d7
Benjamin Egelund-Müller <b@egelund-muller.com>
2020-05-06 15:36:53 +0200

    move conf files to ts

--- web/lib/connection.ts
+++ web/lib/connection.ts
@@ -13,7 +13,7 @@ const GATEWAY_HOST = IS_PRODUCTION ? "data.beneath.dev" : "localhost:5000";
 const GATEWAY_URL = `${HTTP_PROTOCOL}://${GATEWAY_HOST}`;
 const GATEWAY_URL_WS = `${WEBSOCKET_PROTOCOL}://${GATEWAY_HOST}`;
 
-module.exports = {
+export default {
   IS_PRODUCTION,
   HTTP_PROTOCOL,
   WEBSOCKET_PROTOCOL,
