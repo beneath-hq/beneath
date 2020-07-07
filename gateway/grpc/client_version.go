f4794cc28b37dcd2116c3cc9e7a39a971a60b66c
Benjamin Egelund-Müller <b@egelund-muller.com>
2020-07-07 11:13:49 +0200

    minor fixes

--- gateway/grpc/client_version.go
+++ gateway/grpc/client_version.go
@@ -29,5 +29,5 @@ func newVersionOrPanic(str string) *version.Version {
 }
 
 var clientSpecs = map[string]clientVersionSpec{
-	"beneath-python": newClientVersionSpec("1.1.3", "1.1.2", "1.1.1"),
+	"beneath-python": newClientVersionSpec("1.2.0", "1.2.0", "1.1.3"),
 }
