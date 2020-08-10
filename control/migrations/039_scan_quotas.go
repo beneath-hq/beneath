d390590221b412a840682efdd1ad0a477d447b56
greenep12 <ericpgreen2@gmail.com>
2020-08-10 14:21:41 +0200

    migration

--- control/migrations/039_scan_quotas.go
+++ control/migrations/039_scan_quotas.go
@@ -41,9 +41,74 @@ func init() {
 			return err
 		}
 
+		// Organizations.ScanQuota, Organizations.PrepaidScanQuota
+		_, err = db.Exec(`
+			ALTER TABLE organizations
+			ADD scan_quota bigint,
+			ADD prepaid_scan_quota bigint;
+		`)
+		if err != nil {
+			return err
+		}
+
+		_, err = db.Exec(`
+			update organizations
+			set scan_quota = 100000000000,
+					prepaid_scan_quota = 100000000000;
+		`)
+		if err != nil {
+			return err
+		}
+
+		// Users.ScanQuota
+		_, err = db.Exec(`
+			ALTER TABLE users
+			ADD scan_quota bigint;
+		`)
+		if err != nil {
+			return err
+		}
+
+		// Services.ScanQuota
+		_, err = db.Exec(`
+			ALTER TABLE services
+			ADD scan_quota bigint;
+		`)
+		if err != nil {
+			return err
+		}
+
 		// Done
 		return nil
 	}, func(db migrations.DB) (err error) {
+		// Services.ScanQuota
+		_, err = db.Exec(`
+			ALTER TABLE services
+			DROP scan_quota;
+		`)
+		if err != nil {
+			return err
+		}
+
+		// Users.ScanQuota
+		_, err = db.Exec(`
+			ALTER TABLE users
+			DROP scan_quota;
+		`)
+		if err != nil {
+			return err
+		}
+
+		// Organizations.ScanQuota, Organizations.PrepaidScanQuota
+		_, err = db.Exec(`
+			ALTER TABLE organizations
+			DROP scan_quota,
+			DROP prepaid_scan_quota;
+		`)
+		if err != nil {
+			return err
+		}
+
 		// BillingPlan.BaseScanQuota, BillingPlan.SeatScanQuota, BillingPlan.ScanQuota, BillingPlan.ScanOveragePriceCents
 		_, err = db.Exec(`
 			ALTER TABLE billing_plans
