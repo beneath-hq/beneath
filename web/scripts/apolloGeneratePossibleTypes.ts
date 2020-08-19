2a63dbc44c457db6b73d45eb7b77e73bbb8d596c
Benjamin Egelund-Müller <b@egelund-muller.com>
2020-08-19 11:41:06 +0200

    converted scripts to typescript

--- web/scripts/apolloGeneratePossibleTypes.ts
+++ web/scripts/apolloGeneratePossibleTypes.ts
@@ -2,15 +2,14 @@
 // about fragments in our GraphQL schema for Apollo. See this page for details:
 // https://www.apollographql.com/docs/react/data/fragments/#fragments-on-unions-and-interfaces
 
-const connection = require("../lib/connection");
+import { API_URL} from "../lib/connection";
 
-const fetch = require("node-fetch");
-const fs = require("fs");
+import fetch from "node-fetch";
+import fs from "fs";
 
-const API_HOST = connection.API_URL;
 const OUT_FILE = "./apollo/possibleTypes.json";
 
-fetch(`${API_HOST}/graphql`, {
+fetch(`${API_URL}/graphql`, {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
@@ -32,10 +31,10 @@ fetch(`${API_HOST}/graphql`, {
 })
   .then((result) => result.json())
   .then((result) => {
-    const possibleTypes = {};
-    result.data.__schema.types.forEach((supertype) => {
+    const possibleTypes: any = {};
+    result.data.__schema.types.forEach((supertype: any) => {
       if (supertype.possibleTypes) {
-        possibleTypes[supertype.name] = supertype.possibleTypes.map((subtype) => subtype.name);
+        possibleTypes[supertype.name] = supertype.possibleTypes.map((subtype: any) => subtype.name);
       }
     });
 
