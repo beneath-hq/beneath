9c222d0723f4f0908292d7a38497821c2a9486cb
greenep12 <ericpgreen2@gmail.com>
2020-09-29 13:58:31 +0200

    change Welcome page to be like Springboard

--- web/components/console/Welcome.tsx
+++ web/components/console/Welcome.tsx
@@ -1,22 +1,66 @@
-import { Grid, makeStyles, Theme } from "@material-ui/core";
+import { Grid, makeStyles, Theme, Typography } from "@material-ui/core";
+import { ArrowRightAlt, Folder, LinearScale, VpnKey } from "@material-ui/icons";
 import React, { FC } from "react";
 
+import { Link } from "components/Link";
 import ExploreProjectsTiles from "./ExploreProjectsTiles";
 import ActionTile from "./tiles/ActionTile";
-import TitleTile from "./tiles/TitleTile";
 
-const useStyles = makeStyles((theme: Theme) => ({}));
+const useStyles = makeStyles((theme: Theme) => ({
+  sectionTitle: {
+    marginTop: theme.spacing(4),
+  },
+  title: {
+    fontSize: theme.typography.pxToRem(32),
+  },
+}));
 
 const Welcome: FC = () => {
   const classes = useStyles();
 
   return (
-    <Grid container spacing={3} alignItems="stretch" alignContent="stretch">
-      <TitleTile title="Welcome to Beneath!" />
-      <ActionTile title="Create Account" href={`/-/auth`} as={`/-/auth`} />
-      <ActionTile title="Tutorials" href="https://about.beneath.dev/docs/quick-starts/" />
-      <ActionTile title="Contact us" href="https://about.beneath.dev/contact/" />
-      <TitleTile title="Featured projects:" />
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Typography variant="h1" className={classes.title}>Welcome to Beneath!</Typography>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography variant="h3" className={classes.sectionTitle}>
+          Create new
+        </Typography>
+      </Grid>
+      <ActionTile title="Project" href={`/-/auth`} shape="dense">
+        <Folder color="primary" />
+      </ActionTile>
+      <ActionTile title="Stream" href={`/-/auth`} shape="dense">
+        <LinearScale color="primary" />
+      </ActionTile>
+      <ActionTile
+        title="Secret"
+        href={`/-/auth`}
+        shape="dense"
+      >
+        <VpnKey color="primary" />
+      </ActionTile>
+
+      <Grid item xs={12}>
+        <Link href="https://about.beneath.dev/docs/quick-starts/">
+          <Grid container spacing={1} alignItems="center">
+            <Grid item>
+              <Typography variant="h3">Read the quick start</Typography>
+            </Grid>
+            <Grid item>
+              <ArrowRightAlt />
+            </Grid>
+          </Grid>
+        </Link>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography variant="h3" className={classes.sectionTitle}>
+          Featured projects and tutorials
+        </Typography>
+      </Grid>
       <ExploreProjectsTiles />
     </Grid>
   );
