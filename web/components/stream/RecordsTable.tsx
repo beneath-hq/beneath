5e9bf8e1208efebeea52af92cecd751b1fa931d4
greenep12 <ericpgreen2@gmail.com>
2020-10-08 16:18:29 +0200

    add key, schema, description tags; highlight idx

--- web/components/stream/RecordsTable.tsx
+++ web/components/stream/RecordsTable.tsx
@@ -6,8 +6,6 @@ const Moment = dynamic(import("react-moment"), { ssr: false });
 
 import {
   Button,
-  Grid,
-  Icon,
   makeStyles,
   Table,
   TableBody,
@@ -16,10 +14,10 @@ import {
   TableRow,
   Theme,
   Tooltip,
-  Box,
+  Box, Typography, Paper,
+  fade,
 } from "@material-ui/core";
 import InfoIcon from "@material-ui/icons/InfoSharp";
-import SearchIcon from "@material-ui/icons/SearchSharp";
 
 import { Schema } from "./schema";
 import ContentContainer, { ContentContainerProps } from "../ContentContainer";
@@ -31,6 +29,9 @@ const useStyles = makeStyles((theme: Theme) => ({
       borderLeft: "none",
     },
   },
+  keyCell: {
+    backgroundColor: fade(theme.palette.background.default, 0.25),
+  },
   highlightedCell: {
     backgroundColor: "rgba(255, 255, 255, 0.08)",
   },
@@ -40,14 +41,33 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   headerCellContent: {
     display: "inline-flex",
+    alignItems: "center"
   },
   headerCellText: {
     flex: "1",
+    marginRight: theme.spacing(1)
+  },
+  headerCellPaper: {
+    marginLeft: theme.spacing(1),
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(1),
+    cursor: "default"
+  },
+  headerCellPaperInfo: {
+    backgroundColor: theme.palette.border.background,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    height: "24px"
+  },
+  headerCellPaperKey: {
+    backgroundColor: theme.palette.primary.dark,
+  },
+  headerCellPaperType: {
+    backgroundColor: theme.palette.border.background
   },
   headerCellInfo: {
-    fontSize: theme.typography.body1.fontSize,
-    marginLeft: "5px",
-    marginTop: "1px",
+    fontSize: theme.typography.h3.fontSize,
   },
 }));
 
@@ -79,18 +99,23 @@ const RecordsTable: FC<RecordsTableProps> = ({
                   <TableCell key={column.name} className={clsx(classes.cell, classes.headerCell)}>
                     <div className={classes.headerCellContent}>
                       <span className={classes.headerCellText}>{column.displayName}</span>
-                      {column.doc && (
-                        <Tooltip title={column.doc} interactive>
-                          <Icon className={classes.headerCellInfo}>
-                            <InfoIcon fontSize="inherit" />
-                          </Icon>
-                        </Tooltip>
-                      )}
                       {column.isKey && (
                         <Tooltip title={"Column is part of an index"} interactive>
-                          <Icon className={classes.headerCellInfo}>
-                            <SearchIcon fontSize="inherit" />
-                          </Icon>
+                          <Paper className={clsx(classes.headerCellPaper, classes.headerCellPaperKey)}>
+                            <Typography variant="caption">Key</Typography>
+                          </Paper>
+                        </Tooltip>
+                      )}
+                      <Tooltip title={column.typeDescription} interactive>
+                        <Paper className={clsx(classes.headerCellPaper, classes.headerCellPaperType)}>
+                          <Typography variant="caption">{column.typeName}</Typography>
+                        </Paper>
+                      </Tooltip>
+                      {column.doc && (
+                        <Tooltip title={column.doc} interactive>
+                          <Paper className={clsx(classes.headerCellPaper, classes.headerCellPaperInfo)}>
+                            <InfoIcon className={classes.headerCellInfo}/>
+                          </Paper>
                         </Tooltip>
                       )}
                     </div>
@@ -107,14 +132,19 @@ const RecordsTable: FC<RecordsTableProps> = ({
                     hover={true}
                   >
                     {columns.map((column) => (
-                      <TableCell key={column.name} className={classes.cell} align={column.isNumeric ? "right" : "left"}>
-                        {column.type === "timeago" ? (
-                          <Moment fromNow ago date={column.formatRecord(record)} />
-                        ) : (
-                          column.formatRecord(record)
-                        )}
-                      </TableCell>
-                    ))}
+                          <TableCell
+                            key={column.name}
+                            className={clsx(classes.cell, column.isKey && classes.keyCell)}
+                            align={column.isNumeric ? "right" : "left"}
+                          >
+                            {column.type === "timeago" ? (
+                              <Moment fromNow ago date={column.formatRecord(record)} />
+                            ) : (
+                                column.formatRecord(record)
+                              )}
+                          </TableCell>
+                        )
+                    )}
                   </TableRow>
                 ))}
             </TableBody>
