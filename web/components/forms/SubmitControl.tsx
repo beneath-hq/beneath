bb265a3bb2d888fdd4acd06d5c59552205377012
greenep12 <ericpgreen2@gmail.com>
2020-11-17 11:51:18 +0100

    add functionality to SubmitControl

--- web/components/forms/SubmitControl.tsx
+++ web/components/forms/SubmitControl.tsx
@@ -1,9 +1,9 @@
+import { Button, Grid, makeStyles, Theme, Typography } from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import React, { FC } from "react";
 import Moment from "react-moment";
 
-import { Button, makeStyles, Theme, Typography } from "@material-ui/core";
-import { Alert } from "@material-ui/lab";
-
+import clsx from "clsx";
 import FormControl, { FormControlProps } from "./FormControl";
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -13,6 +13,9 @@ const useStyles = makeStyles((theme: Theme) => ({
   button: {
     width: "fit-content", // keep small even in full width form
   },
+  severeButton: {
+    backgroundColor: theme.palette.error.main,
+  },
   timestamps: {
     marginTop: 24,
   },
@@ -20,13 +23,17 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 export interface SubmitControlProps extends FormControlProps {
   label?: string;
+  cancelFn?: () => void;
+  cancelLabel?: string;
+  severe?: boolean;
   errorAlert?: string;
   createdOn?: string;
   updatedOn?: string;
+  rightSide?: boolean;
 }
 
 const SubmitControl: FC<SubmitControlProps> = (props) => {
-  const { label, errorAlert, createdOn, updatedOn, disabled, ...others } = props;
+  const { label, cancelFn, cancelLabel, severe, errorAlert, createdOn, updatedOn, disabled, rightSide, ...others } = props;
   const classes = useStyles();
   return (
     <FormControl disabled={disabled} {...others}>
@@ -35,9 +42,20 @@ const SubmitControl: FC<SubmitControlProps> = (props) => {
           {errorAlert}
         </Alert>
       )}
-      <Button className={classes.button} type="submit" disabled={disabled} variant="contained" color="primary">
-        {label}
-      </Button>
+      <Grid container alignItems="center" spacing={2} justify={rightSide ? "flex-end" : "flex-start"}>
+        {cancelFn && cancelLabel && (
+          <Grid item>
+            <Button className={classes.button} onClick={cancelFn}>
+              {cancelLabel}
+            </Button>
+          </Grid>
+        )}
+        <Grid item>
+          <Button className={clsx(classes.button, severe && classes.severeButton)} type="submit" disabled={disabled} variant="contained" color="primary">
+            {label}
+          </Button>
+        </Grid>
+      </Grid>
       {(createdOn || updatedOn) && (
         <Typography className={classes.timestamps} variant="subtitle1" color="textSecondary">
           {createdOn && (
