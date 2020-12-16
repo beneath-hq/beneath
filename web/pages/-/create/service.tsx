021cad2bf47cc068ad99a7bdc61ba10ba8731dcc
greenep12 <ericpgreen2@gmail.com>
2020-12-16 18:25:24 +0100

    add code to preselect a project

--- web/pages/-/create/service.tsx
+++ web/pages/-/create/service.tsx
@@ -1,13 +1,34 @@
+import { useQuery } from "@apollo/client";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 
 import { withApollo } from "../../../apollo/withApollo";
 import CreateService from "components/service/CreateService";
 import Page from "../../../components/Page";
+import { ProjectByOrganizationAndName, ProjectByOrganizationAndNameVariables } from "apollo/types/ProjectByOrganizationAndName";
+import { QUERY_PROJECT } from "apollo/queries/project";
 
 const CreatePage: NextPage = () => {
+  // Prepopulate query text if &service=... url param is set
+  const router = useRouter();
+  const organizationName = router.query.organization;
+  const projectName = router.query.project;
+  const skip = !(typeof organizationName === "string" && typeof projectName === "string");
+
+  const { loading, error, data } = useQuery<ProjectByOrganizationAndName, ProjectByOrganizationAndNameVariables>(
+    QUERY_PROJECT,
+    {
+      skip,
+      variables: {
+        organizationName: typeof organizationName === "string" ? organizationName : "",
+        projectName: typeof projectName === "string" ? projectName : "",
+      },
+    }
+  );
+
   return (
     <Page title="Create service" contentMarginTop="normal" maxWidth="sm">
-      <CreateService />
+      <CreateService preselectedProject={data?.projectByOrganizationAndName}/>
     </Page>
   );
 };
