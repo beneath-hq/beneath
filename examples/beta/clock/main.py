d22a94ebbcad5eb318cbe98b346b43adc4d11319
greenep12 <ericpgreen2@gmail.com>
2021-02-03 16:57:21 +0000

    structure examples folder

---
+++ examples/beta/clock/main.py
@@ -0,0 +1,17 @@
+import beneath
+from datetime import datetime, timedelta
+
+start =
+
+schema = """
+  
+"""
+
+def generate_frequency(start delta: timedelta):
+
+
+async def generate_clock(p):
+
+  pass
+
+beneath.easy_generate_stream(generator, "clock", "clock")
