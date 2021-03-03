982639c8e3831304bdefa5c1eb6d773852722743
Benjamin Egelund-Müller <b@egelund-muller.com>
2021-03-03 15:38:32 +0100

    small file tidyup

---
+++ doc.go
@@ -0,0 +1,8 @@
+/*
+Package beneath is the top-level package for Beneath, a platform for building
+analytics applications.
+
+For more information, visit https://beneath.dev or browse the source code at
+https://gitlab.com/beneath-hq/beneath.
+*/
+package beneath
