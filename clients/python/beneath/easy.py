0b8a9948894bc7cc230b95bd08dfbf62a924e666
Benjamin Egelund-Müller <b@egelund-muller.com>
2021-03-09 18:52:04 +0100

    (python) easy wrapper for write_full, and docs for easy wrappers

--- clients/python/beneath/easy.py
+++ clients/python/beneath/easy.py
@@ -1,9 +1,12 @@
 from collections.abc import Mapping
-from typing import Iterable
+from typing import Iterable, List, Union
+
+import pandas as pd
 
 from beneath import config
 from beneath.consumer import ConsumerCallback
 from beneath.client import Client
+from beneath.job import Job
 from beneath.pipeline import AsyncApplyFn, AsyncGenerateFn, Pipeline
 
 
@@ -28,6 +31,38 @@ async def consume(
     stop_when_idle: bool = False,
     max_concurrency: int = 1,
 ):
+    """
+    Shorthand for creating a client, creating a stream consumer, replaying the stream's history and
+    subscribing to changes.
+
+    Args:
+        stream_path (str):
+            Path to the stream to subscribe to. The consumer will subscribe to the stream's
+            primary version.
+        cb (async def fn(record)):
+            Async function for processing a record.
+        version (int):
+            The instance version to use for stream. If not set, uses the primary instance.
+        replay_only (bool):
+            If true, will not read changes, but only replay historical records.
+            Defaults to False.
+        changes_only (bool):
+            If true, will not replay historical records, but only subscribe to new changes.
+            Defaults to False.
+        subscription_path (str):
+            Format "ORGANIZATION/PROJECT/NAME". If set, the consumer will use a checkpointer
+            to save cursors. That means processing will not restart from scratch if the process
+            ends or crashes (as long as you use the same subscription name). To reset a
+            subscription, call ``reset`` on the consumer.
+        reset_subscription (bool):
+            If true and ``subscription_path`` is set, will reset the consumer and start the
+            subscription from scratch.
+        stop_when_idle (bool):
+            If true, will return when "caught up" and no new changes are available.
+            Defaults to False.
+        max_concurrency (int):
+            The maximum number of callbacks to call concurrently. Defaults to 1.
+    """
     client = _get_client()
     consumer = await client.consumer(
         stream_path=stream_path,
@@ -55,6 +90,36 @@ async def query_index(
     batch_size=config.DEFAULT_READ_BATCH_SIZE,
     warn_max=True,
 ) -> Iterable[Mapping]:
+    """
+    Shorthand for creating a client, finding a stream, and reading (filtered) records from it.
+
+    Args:
+        stream_path (str):
+            The path to the stream in the format of "USERNAME/PROJECT/STREAM"
+        version (int):
+            The instance version to read from. If not set, defaults to the stream's primary
+            instance.
+        filter (str):
+            A filter to apply to the index. Filters allow you to quickly
+            find specific record(s) in the index based on the record key.
+            For details on the filter syntax,
+            see https://about.beneath.dev/docs/reading-writing-data/index-filters/.
+        to_dataframe (bool):
+            If true, will return the result as a Pandas dataframe. Defaults to true.
+        max_bytes (int)
+            Sets the maximum number of bytes to read before returning with a warning.
+            Defaults to 10 MB (Avro-encoded).
+        max_records (int):
+            Sets the maximum number of records to read before returning with a warning.
+            Defaults to unlimited (see ``max_bytes``).
+        batch_size (int):
+            Sets the number of records to fetch in each network request. Defaults to 1000.
+            One call to ``query_index`` may make many network requests (until all records
+            have been loaded or ``max_bytes`` or ``max_records`` is breached).
+        warn_max (bool):
+            If true, will emit a warning if ``max_bytes`` or ``max_records`` were breached and
+            the function returned without loading the full result. Defaults to true.
+    """
     client = _get_client()
     stream = await client.find_stream(stream_path)
     if version is None:
@@ -82,11 +147,44 @@ async def query_warehouse(
     max_records=None,
     batch_size=config.DEFAULT_READ_BATCH_SIZE,
     warn_max=True,
-) -> Iterable[Mapping]:
+) -> Union[Iterable[Mapping], Job]:
+    """
+    Shorthand for creating a client, running a warehouse (OLAP) query, and reading the result.
+    If analyze=True, the analyzed job is returned instead of the result.
+
+    Args:
+        query (str):
+            The analytical SQL query to run. To learn about the query language,
+            see https://about.beneath.dev/docs/reading-writing-data/warehouse-queries/.
+        analyze (bool):
+            If true, analyzes the query and returns info about referenced streams
+            and expected bytes scanned, but doesn't actually run the query.
+        max_bytes_scanned (int):
+            Sets a limit on the number of bytes the query can scan.
+            If exceeded, the job will fail with an error.
+            Defaults to 10GB.
+        to_dataframe (bool):
+            If true, will return the result as a Pandas dataframe. Defaults to true.
+        max_bytes (int)
+            Sets the maximum number of bytes to read before returning with a warning.
+            Defaults to 10 MB (Avro-encoded).
+        max_records (int):
+            Sets the maximum number of records to read before returning with a warning.
+            Defaults to unlimited (see ``max_bytes``).
+        batch_size (int):
+            Sets the number of records to fetch in each network request. Defaults to 1000.
+            One call to ``query_index`` may make many network requests (until all records
+            have been loaded or ``max_bytes`` or ``max_records`` is breached).
+        warn_max (bool):
+            If true, will emit a warning if ``max_bytes`` or ``max_records`` were breached and
+            the function returned without loading the full result. Defaults to true.
+    """
     client = _get_client()
     job = await client.query_warehouse(
         query=sql, analyze=analyze, max_bytes_scanned=max_bytes_scanned
     )
+    if analyze:
+        return job
     cursor = await job.get_cursor()
     return await cursor.read_all(
         to_dataframe=to_dataframe,
@@ -95,6 +193,50 @@ async def query_warehouse(
         batch_size=batch_size,
         warn_max=warn_max,
     )
+
+
+async def write_full(
+    stream_path: str,
+    records: Union[Iterable[dict], pd.DataFrame],
+    key: Union[str, List[str]] = None,
+    description: str = None,
+    recreate_on_schema_change=False,
+):
+    """
+    Shorthand for creating a client, inferring a stream schema, creating a stream, and writing
+    data to the stream. It wraps ``Client.write_full``. Calling ``write_full`` multiple times
+    for the same stream, will create and write to multiple versions, but will not append data
+    an existing version.
+
+    Args:
+        stream_path (str):
+            The (desired) path to the stream in the format of "USERNAME/PROJECT/STREAM".
+            The project must already exist.
+        records (list(dict) | pandas.DataFrame):
+            The full dataset to write, either as a list of records or as a Pandas DataFrame.
+            This function uses ``beneath.infer_avro`` to infer a schema for the stream based
+            on the records.
+        key (str | list(str)):
+            The fields to use as the stream's key. If not set, will default to the dataframe
+            index if ``records`` is a Pandas DataFrame, or add a column of incrementing numbers
+            if ``records`` is a list.
+        description (str):
+            A description for the stream.
+        recreate_on_schema_change (bool):
+            If true, and there's an existing stream at ``stream_path`` with a schema that is
+            incompatible with the inferred schema for ``records``, it will delete the existing
+            stream and create a new one instead of throwing an error. Defaults to false.
+    """
+    client = _get_client()
+    await client.start()
+    await client.write_full(
+        stream_path=stream_path,
+        records=records,
+        key=key,
+        description=description,
+        recreate_on_schema_change=recreate_on_schema_change,
+    )
+    await client.stop()
 
 
 def generate_stream_pipeline(
