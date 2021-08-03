60531ea32595346ea14b4a9d7ebd71da46b900cc
greenep12 <ericpgreen2@gmail.com>
2021-08-03 09:08:37 +0000

    create table outside of aggregate script

--- examples/wallstreetbets-analytics/aggregates/aggregate.py
+++ examples/wallstreetbets-analytics/aggregates/aggregate.py
@@ -5,25 +5,9 @@ from datetime import datetime, timedelta
 
 async def main():
     client = beneath.Client()
-
-    table = await client.create_table(
-        "examples/wallstreetbets-analytics/stock-mentions-rollup-daily",
-        schema="""
-      " A rollup table of stock mentions "
-      type MentionDay @schema {
-          symbol: String! @key
-          day: Timestamp! @key
-          num_mentions: Int32!
-          num_positive: Int32!
-          num_neutral: Int32!
-          num_negative: Int32!
-          avg_polarity: Float32!
-          avg_subjectivity: Float32!
-      }
-  """,
-        update_if_exists=True,
+    table = await client.find_table(
+        "examples/wallstreetbets-analytics/stock-mentions-rollup-daily"
     )
-
     yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
     mentions = await beneath.query_warehouse(
         f"""
