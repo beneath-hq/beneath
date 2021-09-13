d08db56d574a43b3909faa87af6bd49cf061d6fc
greenep12 <ericpgreen2@gmail.com>
2021-09-13 09:12:50 +0000

    Create a test environment

---
+++ examples/cdc-postgres/create_testdb.py
@@ -0,0 +1,61 @@
+import psycopg2
+import yaml
+
+if __name__ == "__main__":
+    # load config
+    with open(".development.yaml", "r") as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+
+    # connect to "postgres" db
+    # connecting to "postgres" db allows me to drop & create other dbs
+    conn = psycopg2.connect(
+        database="postgres",
+        user=cfg["postgres"]["username"],
+        password=cfg["postgres"]["password"],
+        host=cfg["postgres"]["host"],
+        port="5432",
+    )
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    # create a test database
+    cursor.execute("DROP DATABASE IF EXISTS testdb")
+    cursor.execute("CREATE DATABASE testdb")
+    print("Created test database.")
+
+    # close "postgres", open "testdb"
+    conn.close()
+    conn = psycopg2.connect(
+        database="testdb",
+        user=cfg["postgres"]["username"],
+        password=cfg["postgres"]["password"],
+        host=cfg["postgres"]["host"],
+        port="5432",
+    )
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    # create user table
+    cursor.execute(
+        """
+    CREATE TABLE users (
+        user_id int,
+        email text,
+        authorized boolean,
+        created_on timestamptz, 
+        updated_on timestamptz
+    );
+    """
+    )
+    print("Created user table.")
+
+    # populate with data
+    cursor.execute(
+        """
+    INSERT INTO users VALUES (1, 'john@test.com', False);
+      """
+    )
+    print("Inserted a dummy record.")
+
+    # close testdb connection
+    conn.close()
