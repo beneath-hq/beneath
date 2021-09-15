e9a32eb123e3b1dc54bdae72b26fa51e1d106f27
greenep12 <ericpgreen2@gmail.com>
2021-09-15 15:40:26 +0000

    Consolidate tests

--- examples/beta/cdc-postgres/test/create_source_db.py
+++ examples/beta/cdc-postgres/test/create_source_db.py
@@ -4,50 +4,24 @@ import yaml
 if __name__ == "__main__":
     # load config
     with open(".development.yaml", "r") as ymlfile:
-        cfg = yaml.safe_load(ymlfile)
+        config = yaml.safe_load(ymlfile)
 
     # connect to "postgres" db
     # connecting to "postgres" db allows me to drop & create other dbs
     conn = psycopg2.connect(
         database="postgres",
-        user=cfg["postgres"]["username"],
-        password=cfg["postgres"]["password"],
-        host=cfg["postgres"]["host"],
+        user=config["postgres"]["username"],
+        password=config["postgres"]["password"],
+        host=config["postgres"]["host"],
         port="5432",
     )
     conn.autocommit = True
     cursor = conn.cursor()
 
     # create a test database
-    cursor.execute("DROP DATABASE IF EXISTS testdb")
-    cursor.execute("CREATE DATABASE testdb")
+    cursor.execute(f"DROP DATABASE IF EXISTS {config['postgres']['database']}")
+    cursor.execute(f"CREATE DATABASE {config['postgres']['database']}")
     print("Created test database.")
 
-    # close "postgres", open "testdb"
-    conn.close()
-    conn = psycopg2.connect(
-        database="testdb",
-        user=cfg["postgres"]["username"],
-        password=cfg["postgres"]["password"],
-        host=cfg["postgres"]["host"],
-        port="5432",
-    )
-    conn.autocommit = True
-    cursor = conn.cursor()
-
-    # create user table
-    cursor.execute(
-        """
-    CREATE TABLE users (
-        user_id int,
-        email text,
-        authorized boolean,
-        created_on timestamptz, 
-        updated_on timestamptz
-    );
-    """
-    )
-    print("Created user table.")
-
-    # close testdb connection
+    # close connection
     conn.close()
--- examples/beta/cdc-postgres/test/operate_on_source_db.py
+++ examples/beta/cdc-postgres/test/operate_on_source_db.py
@@ -4,6 +4,7 @@ import string
 import os
 import sys
 import inspect
+import yaml
 
 # add parent directory to path, so I can import from the cdc.py module
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -12,7 +13,42 @@ sys.path.insert(0, parentdir)
 
 from cdc import connect_to_source_db
 
+with open(".development.yaml", "r") as ymlfile:
+    config = yaml.safe_load(ymlfile)
+
 SLEEP_INTERVAL = 10  # seconds
+OUTPUT_PLUGIN = "pgoutput"  # or "wal2json"
+
+
+def create_table(cursor):
+    cursor.execute(
+        """
+    CREATE TABLE users (
+        user_id int,
+        email text,
+        authorized boolean,
+        created_on timestamptz, 
+        updated_on timestamptz
+    );
+    """
+    )
+
+
+def drop_table(cursor):
+    cursor.execute("DROP TABLE IF EXISTS users;")
+
+
+def do_everything_txn(cursor):
+    cursor.execute(
+        f"""
+    DROP TABLE IF EXISTS actions;
+    CREATE TABLE actions (a integer primary key);
+    INSERT INTO actions (a) VALUES(1);
+    UPDATE actions SET a = 2 WHERE a = 1;
+    DELETE FROM actions WHERE a = 2;
+    TRUNCATE TABLE actions;
+    """
+    )
 
 
 def insert(cursor):
@@ -42,17 +78,36 @@ def delete(cursor):
     print(f"Deleted users where email starts with {username}")
 
 
-def pick_op(cursor):
+def pick_random_op(cursor):
     random.choice([insert, update, delete])(cursor)
 
 
-async def do_things(conn):
-    cursor = conn.cursor()
+async def do_things_continuously(cursor):
     while True:
-        pick_op(cursor)
+        pick_random_op(cursor)
         await asyncio.sleep(SLEEP_INTERVAL)
 
 
+def get_changes(cursor):
+    cursor.execute(
+        f"SELECT data FROM pg_logical_slot_get_changes('{config['postgres']['replication_slot']}', NULL, NULL);"
+    )
+    changes = cursor.fetchall()
+    return changes
+
+
 if __name__ == "__main__":
+    # setup
     conn = connect_to_source_db()
-    asyncio.run(do_things(conn))
+    cursor = conn.cursor()
+    create_table(cursor)
+
+    # operate
+    do_everything_txn(cursor)
+    # asyncio.run(do_things_continuously(cursor))
+
+    # inspect
+    # print(get_changes(cursor))
+
+    # cleanup
+    drop_table(cursor)
