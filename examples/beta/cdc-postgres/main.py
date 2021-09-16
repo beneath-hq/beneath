07fcfcd2863d078c1d0b508cf12b33e4504b8cb5
greenep12 <ericpgreen2@gmail.com>
2021-09-16 14:59:13 +0000

    Get WAL timestamps, filter WAL for select tables

--- examples/beta/cdc-postgres/main.py
+++ examples/beta/cdc-postgres/main.py
@@ -4,16 +4,17 @@ import psycopg2
 import json
 import yaml
 from datetime import datetime
+from schemas import get_schema
 
 with open(".development.yaml", "r") as ymlfile:
     config = yaml.safe_load(ymlfile)
 
-POLLING_INTERVAL = 30
+POLLING_INTERVAL = 5
 SCHEMA = """
 type Table @schema {
     table: String! @key
-    operation: String! @key
     timestamp: Timestamp! @key
+    operation: String! @key
     value: String!
 }
 """
@@ -38,12 +39,15 @@ cursor = conn.cursor()
 async def get_changes(p):
     while True:
         cursor.execute(
-            f"SELECT data FROM pg_logical_slot_get_changes('{config['postgres']['replication_slot']}', NULL, NULL);"
+            f"""
+        SELECT data FROM pg_logical_slot_get_changes('{config['postgres']['replication_slot']}', NULL, NULL,
+        'include-lsn', 'True', 'include-timestamp', 'True', 'add-tables', '{','.join(config['postgres']['tables'])}');
+        """
         )
-        data = cursor.fetchall()
-        for txn in data:
-            # TODO: what is txn[1] for? when does it get used?
-            changes = json.loads(txn[0])["change"]
+        txns = cursor.fetchall()
+        for txn in txns:
+            txn_json = json.loads(txn[0])
+            changes = txn_json["change"]
             for change in changes:
                 value_dict = {}
                 if change["kind"] == "insert":
@@ -59,29 +63,35 @@ async def get_changes(p):
                     value_dict["oldkeys"] = change["oldkeys"]
                 yield {
                     "table": change["table"],
+                    "timestamp": datetime.strptime(
+                        f"{txn_json['timestamp']}00", "%Y-%m-%d %H:%M:%S.%f%z"
+                    ),
                     "operation": change["kind"],
-                    "timestamp": datetime.now(),
                     "value": json.dumps(value_dict),
                 }
+        # TODO: consider checkpointing the LSN (but pg_logical_slot_get_changes() doesn't let us choose LSN position)
+        # p.checkpoints.set("nextlsn", txn_json["nextlsn"])
         await asyncio.sleep(POLLING_INTERVAL)
 
 
 def filter_for_table(table):
     async def filter(record):
         if record["table"] == table:
+            # TODO: convert general CDC record to table-specific schema
             yield record
 
     return filter
 
 
 def fan_out(p, all_changes, list_of_tables):
-    for table in list_of_tables:
+    # list_of_tables: ["schemaA.table1", "schemaA.table2", "schemaB.table1", ...]
+    for schema_table in list_of_tables:
+        table = schema_table.split(".")[1]
         table_changes = p.apply(all_changes, filter_for_table(table))
-        # TODO: figure out schemas
         p.write_table(
             table_changes,
             f"{config['beneath']['username']}/{config['beneath']['project']}/{table}-changes",
-            schema=SCHEMA,
+            schema=get_schema(cursor, table),
             description=f"Fan-out table ({table}) created by a Postgres CDC service",
         )
 
