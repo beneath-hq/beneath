a1f1e1eacd0e28be970ef2762dde8e89d4c02f2c
greenep12 <ericpgreen2@gmail.com>
2021-09-22 12:13:52 +0000

    Add tests: required columns, more types, pgoutput

--- examples/beta/cdc-postgres/test/operate_on_source_db.py
+++ examples/beta/cdc-postgres/test/operate_on_source_db.py
@@ -42,9 +42,9 @@ def create_table_many_types(cursor, table):
         f"""
     CREATE TABLE {table} (
         id int primary key,
-        text text,
-        bool boolean,
-        timestamp timestamptz
+        text text NOT NULL,
+        bool boolean NOT NULL,
+        timestamp timestamptz NOT NULL
     );
         """
     )
@@ -52,6 +52,14 @@ def create_table_many_types(cursor, table):
 
 def insert_many_types(cursor, table):
     cursor.execute(f"INSERT INTO {table} VALUES (1, 'abc', True, now());")
+
+
+def update_many_types(cursor, table):
+    cursor.execute(f"UPDATE {table} SET text = 'def', timestamp = now() WHERE id = 1;")
+
+
+def delete_many_types(cursor, table):
+    cursor.execute(f"DELETE FROM {table} WHERE id = 1;")
 
 
 def create_table_compound_pk(cursor, table):
@@ -65,6 +73,19 @@ def create_table_compound_pk(cursor, table):
     );
     """
     )
+
+
+# TODO: test tables with compound pks
+def insert_compound_pk(cursor, table):
+    ...
+
+
+def update_compound_pk(cursor, table):
+    ...
+
+
+def delete_compound_pk(cursor, table):
+    ...
 
 
 def drop_table(cursor, table):
@@ -98,12 +119,22 @@ def get_changes(cursor):
 
 
 # used for pgoutput plugin
-# TODO: figure out if I can decode the binary data into useful text data
-# TODO: Debezium somehow decodes the binary. Look into running Debezium in an "embedded" mode (I think that's what Airbyte does).
+# TODO: figure out if I can decode the binary data into useful text data. Debezium somehow decodes it.
 def get_binary_changes(cursor):
     cursor.execute(
         f"""
         SELECT data FROM pg_logical_slot_get_binary_changes('{config['postgres']['replication_slot'] + "_pgoutput"}', NULL, NULL,
+        'proto_version', '1', 'publication_names', 'beneath_publication');
+        """
+    )
+    changes = cursor.fetchall()
+    return changes
+
+
+def peek_binary_changes(cursor):
+    cursor.execute(
+        f"""
+        SELECT data FROM pg_logical_slot_peek_binary_changes('{config['postgres']['replication_slot'] + "_pgoutput"}', NULL, NULL,
         'proto_version', '1', 'publication_names', 'beneath_publication');
         """
     )
@@ -117,16 +148,20 @@ if __name__ == "__main__":
     drop_table(cursor, "table1")
     drop_table(cursor, "table2")
     create_table_simple(cursor, "table1")
-    create_table_simple(cursor, "table2")
+    create_table_many_types(cursor, "table2")
 
     # operate
     insert_simple(cursor, "table1")
+    insert_many_types(cursor, "table2")
     update_simple(cursor, "table1")
-    # delete_simple(cursor, "table1")
+    update_many_types(cursor, "table2")
+    delete_simple(cursor, "table1")
+    # delete_many_types(cursor, "table2")
 
     # inspect
     # print(get_changes(cursor))
-    print(get_binary_changes(cursor))
+    # print(get_binary_changes(cursor))
+    # print(peek_binary_changes(cursor))
 
     # cleanup
     # drop_table(cursor, "table1")
