c58e544bb07426c032863b048c53d982a90c2768
greenep12 <ericpgreen2@gmail.com>
2021-12-03 16:17:29 +0000

    Update tests

--- examples/beta/cdc-postgres/test/operate_on_source_db.py
+++ examples/beta/cdc-postgres/test/operate_on_source_db.py
@@ -1,21 +1,25 @@
-import inspect
-import sys
-import os
+import psycopg2
 import yaml
-
-# add parent directory to path, so I can import from the main.py module
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir)
-
-from main import connect_to_source_db
 
 with open(".development.yaml", "r") as ymlfile:
     config = yaml.safe_load(ymlfile)
 
 
-def create_table_simple(cursor, table):
-    cursor.execute(
+def connect_to_source_db():
+    conn = psycopg2.connect(
+        database=config["postgres"]["database"],
+        user=config["postgres"]["username"],
+        password=config["postgres"]["password"],
+        host=config["postgres"]["host"],
+        port="5432",
+    )
+    conn.autocommit = True
+    cur = conn.cursor()
+    return cur
+
+
+def create_table_simple(cur, table):
+    cur.execute(
         f"""
     CREATE TABLE {table} (
         id int primary key,
@@ -25,23 +29,24 @@ def create_table_simple(cursor, table):
     )
 
 
-def insert_simple(cursor, table):
-    cursor.execute(f"INSERT INTO {table} VALUES (1, 'abc');")
+def insert_simple(cur, table):
+    cur.execute(f"INSERT INTO {table} VALUES (1, 'abc');")
 
 
-def update_simple(cursor, table):
-    cursor.execute(f"UPDATE {table} SET val = 'def' WHERE id = 1;")
+def update_simple(cur, table):
+    cur.execute(f"UPDATE {table} SET val = 'def' WHERE id = 1;")
 
 
-def delete_simple(cursor, table):
-    cursor.execute(f"DELETE FROM {table} WHERE id = 1;")
+def delete_simple(cur, table):
+    cur.execute(f"DELETE FROM {table} WHERE id = 1;")
 
 
-def create_table_many_types(cursor, table):
-    cursor.execute(
+def create_table_many_types(cur, table):
+    cur.execute(
         f"""
     CREATE TABLE {table} (
-        id int primary key,
+        uuid uuid primary key,
+        number int NOT NULL,
         text text NOT NULL,
         bool boolean NOT NULL,
         timestamp timestamptz NOT NULL
@@ -50,20 +55,22 @@ def create_table_many_types(cursor, table):
     )
 
 
-def insert_many_types(cursor, table):
-    cursor.execute(f"INSERT INTO {table} VALUES (1, 'abc', True, now());")
+def insert_many_types(cur, table):
+    cur.execute(
+        f"INSERT INTO {table} VALUES (uuid_generate_v4(), 1, 'abc', True, now());"
+    )
 
 
-def update_many_types(cursor, table):
-    cursor.execute(f"UPDATE {table} SET text = 'def', timestamp = now() WHERE id = 1;")
+def update_many_types(cur, table):
+    cur.execute(f"UPDATE {table} SET text = 'def', timestamp = now() WHERE number = 1;")
 
 
-def delete_many_types(cursor, table):
-    cursor.execute(f"DELETE FROM {table} WHERE id = 1;")
+def delete_many_types(cur, table):
+    cur.execute(f"DELETE FROM {table} WHERE number = 1;")
 
 
-def create_table_compound_pk(cursor, table):
-    cursor.execute(
+def create_table_compound_pk(cur, table):
+    cur.execute(
         f"""
     CREATE TABLE {table} (
         id_1 int,
@@ -75,27 +82,21 @@ def create_table_compound_pk(cursor, table):
     )
 
 
-# TODO: test tables with compound pks
-def insert_compound_pk(cursor, table):
-    ...
+def insert_compound_pk(cur, table):
+    cur.execute(f"INSERT INTO {table} VALUES (1, 1, 'abc');")
 
 
-def update_compound_pk(cursor, table):
-    ...
+def update_compound_pk(cur, table):
+    cur.execute(f"UPDATE {table} SET val = 'def' WHERE id_1 = 1 AND id_2 = 1;")
 
 
-def delete_compound_pk(cursor, table):
-    ...
+def delete_compound_pk(cur, table):
+    cur.execute(f"DELETE FROM {table} WHERE id_1 = 1 AND id_2 = 1;")
 
 
-def drop_table(cursor, table):
-    cursor.execute(f"DROP TABLE IF EXISTS {table};")
-
-
-def do_everything_txn(cursor, table):
-    cursor.execute(
+def do_everything_txn(cur, table):
+    cur.execute(
         f"""
-    DROP TABLE IF EXISTS {table};
     CREATE TABLE {table} (a integer primary key);
     INSERT INTO {table} (a) VALUES(1);
     UPDATE {table} SET a = 2 WHERE a = 1;
@@ -105,64 +106,26 @@ def do_everything_txn(cursor, table):
     )
 
 
-# used for wal2json plugin
-# TODO: update wal2json plugin, so I can include more parameters and get more data. Document the process.
-def get_changes(cursor):
-    cursor.execute(
-        f"""
-        SELECT data FROM pg_logical_slot_get_changes('{config['postgres']['replication_slot']}', NULL, NULL,
-        'include-lsn', 'True', 'include-timestamp', 'True', 'add-tables', 'public.table1, public.table2');
-        """
-    )
-    changes = cursor.fetchall()
-    return changes
-
-
-# used for pgoutput plugin
-# TODO: figure out if I can decode the binary data into useful text data. Debezium somehow decodes it.
-def get_binary_changes(cursor):
-    cursor.execute(
-        f"""
-        SELECT data FROM pg_logical_slot_get_binary_changes('{config['postgres']['replication_slot'] + "_pgoutput"}', NULL, NULL,
-        'proto_version', '1', 'publication_names', 'beneath_publication');
-        """
-    )
-    changes = cursor.fetchall()
-    return changes
-
-
-def peek_binary_changes(cursor):
-    cursor.execute(
-        f"""
-        SELECT data FROM pg_logical_slot_peek_binary_changes('{config['postgres']['replication_slot'] + "_pgoutput"}', NULL, NULL,
-        'proto_version', '1', 'publication_names', 'beneath_publication');
-        """
-    )
-    changes = cursor.fetchall()
-    return changes
+def drop_table(cur, table):
+    cur.execute(f"DROP TABLE IF EXISTS {table};")
 
 
 if __name__ == "__main__":
     # setup
-    cursor = connect_to_source_db()
-    drop_table(cursor, "table1")
-    drop_table(cursor, "table2")
-    create_table_simple(cursor, "table1")
-    create_table_many_types(cursor, "table2")
+    cur = connect_to_source_db()
+    create_table_simple(cur, "table1")
+    create_table_many_types(cur, "table2")
 
     # operate
-    insert_simple(cursor, "table1")
-    insert_many_types(cursor, "table2")
-    update_simple(cursor, "table1")
-    update_many_types(cursor, "table2")
-    delete_simple(cursor, "table1")
-    # delete_many_types(cursor, "table2")
-
-    # inspect
-    # print(get_changes(cursor))
-    # print(get_binary_changes(cursor))
-    # print(peek_binary_changes(cursor))
+    insert_simple(cur, "table1")
+    update_simple(cur, "table1")
+    delete_simple(cur, "table1")
+    insert_many_types(cur, "table2")
+    update_many_types(cur, "table2")
+    delete_many_types(cur, "table2")
+    do_everything_txn(cur, "table3")
 
     # cleanup
-    # drop_table(cursor, "table1")
-    # drop_table(cursor, "table2")
+    drop_table(cur, "table1")
+    drop_table(cur, "table2")
+    drop_table(cur, "table3")
