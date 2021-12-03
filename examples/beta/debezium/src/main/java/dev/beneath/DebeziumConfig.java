976d5ce555f3754f4ba8c651464a94a3f738a8ed
greenep12 <ericpgreen2@gmail.com>
2021-12-03 16:01:50 +0000

    Change Beneath project and table names

--- examples/beta/debezium/src/main/java/dev/beneath/DebeziumConfig.java
+++ examples/beta/debezium/src/main/java/dev/beneath/DebeziumConfig.java
@@ -1,9 +1,6 @@
 package dev.beneath;
 
 public class DebeziumConfig {
-  public static final String BENEATH_SECRET = "7HsGdXeNUygRtsZKcLGmutouBz83Fp6ksfmW3LyG2GUa";
-  public static final String BENEATH_PROJECT_PATH = "ericpgreen2/debezium";
-  public static final String BENEATH_DEBEZIUM_ROOT_TABLE_PATH = BENEATH_PROJECT_PATH + "/postgres_changes";
   public static final String DATABASE_HOSTNAME = "host.docker.internal";
   public static final String DATABASE_PORT = "5432";
   public static final String DATABASE_USER = "ericgreen";
@@ -11,4 +8,8 @@ public class DebeziumConfig {
   public static final String DATABASE_DBNAME = "testdb";
   public static final String TABLE_INCLUDE_LIST = "public.table1, public.table2, public.table3";
   public static final String DATABASE_SERVER_NAME = "testserver";
+  public static final String BENEATH_SECRET = "7HsGdXeNUygRtsZKcLGmutouBz83Fp6ksfmW3LyG2GUa";
+  public static final String BENEATH_USERNAME = "ericpgreen2";
+  public static final String BENEATH_PROJECT_PATH = BENEATH_USERNAME + "/debezium-postgres-" + DATABASE_DBNAME;
+  public static final String BENEATH_DEBEZIUM_ROOT_TABLE_PATH = BENEATH_PROJECT_PATH + "/raw_changes";
 }
