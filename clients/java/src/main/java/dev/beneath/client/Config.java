9d9ff81ed6bdae9c1442fffa8ad2cf95149511f7
greenep12 <ericpgreen2@gmail.com>
2022-01-17 19:31:07 +0000

    Add prod/dev config toggle

--- clients/java/src/main/java/dev/beneath/client/Config.java
+++ clients/java/src/main/java/dev/beneath/client/Config.java
@@ -1,7 +1,9 @@
 package dev.beneath.client;
 
+import java.util.Arrays;
+
 public class Config {
-  public static final Boolean DEV = true;
+  public static final Boolean DEV = Arrays.asList("dev", "development").contains(System.getenv("BENEATH_ENV"));
 
   public static final String JAVA_CLIENT_ID = "beneath-java";
   public static final String JAVA_CLIENT_VERSION = "0.1.0";
@@ -13,8 +15,21 @@ public class Config {
   public static final Integer MAX_BATCH_SIZE_BYTES = 10000000;
   public static final Integer MAX_BATCH_SIZE_COUNT = 10000;
 
-  public static final String BENEATH_FRONTEND_HOST = "http://host.docker.internal:3000";
-  public static final String BENEATH_CONTROL_HOST = "http://host.docker.internal:4000/graphql";
-  public static final String BENEATH_GATEWAY_HOST = "http://host.docker.internal:5000";
-  public static final String BENEATH_GATEWAY_HOST_GRPC = "host.docker.internal:50051";
+  public static String BENEATH_FRONTEND_HOST;
+  public static String BENEATH_CONTROL_HOST;
+  public static String BENEATH_GATEWAY_HOST;
+  public static String BENEATH_GATEWAY_HOST_GRPC;
+  static {
+    if (DEV) {
+      BENEATH_FRONTEND_HOST = "http://host.docker.internal:3000";
+      BENEATH_CONTROL_HOST = "http://host.docker.internal:4000/graphql";
+      BENEATH_GATEWAY_HOST = "http://host.docker.internal:5000";
+      BENEATH_GATEWAY_HOST_GRPC = "host.docker.internal:50051";
+    } else {
+      BENEATH_FRONTEND_HOST = "https://beneath.dev";
+      BENEATH_CONTROL_HOST = "https://control.beneath.dev";
+      BENEATH_GATEWAY_HOST = "https://data.beneath.dev";
+      BENEATH_GATEWAY_HOST_GRPC = "grpc.data.beneath.dev";
+    }
+  }
 }
